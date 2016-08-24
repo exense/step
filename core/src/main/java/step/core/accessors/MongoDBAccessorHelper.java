a7e2476ccfb97811b551d55cd4ed25921b1053f7
Jérôme Comte <jcomte@denkbar.io>
2016-08-24 14:13:10 +0200

    Adding MongoDB configuration params.

--- core/src/main/java/step/core/accessors/MongoDBAccessorHelper.java
+++ core/src/main/java/step/core/accessors/MongoDBAccessorHelper.java
@@ -1,14 +1,18 @@
 package step.core.accessors;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
 
-import step.commons.conf.Configuration;
-
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+
+import step.commons.conf.Configuration;
 
 public class MongoDBAccessorHelper {
 
@@ -16,42 +20,54 @@ public class MongoDBAccessorHelper {
 
 	private String host;
 	
+	private Integer port;
+	
+	String user;
+	
+	String pwd;
+	
+	String db;
+	
 	public MongoDBAccessorHelper() {
 		super();
 		
 		host = Configuration.getInstance().getProperty("db.host");
+		port = Configuration.getInstance().getPropertyAsInteger("db.port",27017);
+		user = Configuration.getInstance().getProperty("db.username");
+		pwd = Configuration.getInstance().getProperty("db.password");
+		db = Configuration.getInstance().getProperty("db.database","step");
 	}
 
 	public static MongoDBAccessorHelper getInstance() {
 		return INSTANCE;
 	}
 	
-	public MongoCollection getCollection(String collectionName) {
-		MongoClient mongoClient;
-		try {
-			mongoClient = new MongoClient(host);
-		} catch (UnknownHostException e) {
-			throw new RuntimeException(e);
-		}
-		DB db = mongoClient.getDB( "step" );
-		
-		Jongo jongo = new Jongo(db);
-		MongoCollection reports = jongo.getCollection(collectionName);
-		
-		return reports;
+	public static MongoClient getClient() {
+		return getInstance().getMongoClient();
 	}
 	
-	public static MongoClient getClient() {
+	public MongoClient getMongoClient() {
 		try {
-			String host = Configuration.getInstance().getProperty("db.host");
-			return new MongoClient(host);
-		} catch (UnknownHostException e) {
-			throw new RuntimeException(e);
+			ServerAddress address = new ServerAddress(host, port);
+			List<MongoCredential> credentials = new ArrayList<>();
+			if(user!=null) {
+				MongoCredential credential = MongoCredential.createMongoCRCredential(user, db, pwd.toCharArray());
+				credentials.add(credential);
+			}
+			return new MongoClient(address, credentials);
+		} catch (UnknownHostException e1) {
+			throw new RuntimeException(e1);
 		}
+		
+		
 	}
 	
 	public static MongoCollection getCollection(MongoClient client, String collectionName) {
-		DB db = client.getDB( "step" );
+		return getInstance().getMongoCollection(client, collectionName);
+	}
+	
+	public MongoCollection getMongoCollection(MongoClient client, String collectionName) {
+		DB db = client.getDB(this.db);
 		
 		Jongo jongo = new Jongo(db);
 		MongoCollection collection = jongo.getCollection(collectionName);
