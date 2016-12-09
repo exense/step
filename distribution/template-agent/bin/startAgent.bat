cbe54a1cde833209c615df3c4a8b658b442ad02a
Jérôme Comte <jcomte@denkbar.io>
2016-12-09 16:37:03 +0100

    Removing windows build

--- distribution/template-agent/bin/startAgent.bat
+++ distribution/template-agent/bin/startAgent.bat
@@ -19,10 +19,10 @@
 rem @echo off
 
 rem if java.exe isn't on your path or is too old, then set your own as follows (watch for the backslash and double quote at the end):
-rem  SET JAVA_PATH="D:\Program Files\Java\jdk1.8.0_73\jre\bin"\
+rem SET JAVA_PATH=C:\Program Files\Java\jdk1.8.0_101\bin\
 SET JAVA_PATH=
 
 SET JAVA_OPTS=-Dlogback.configurationFile=./logback.xml
 
-%JAVA_PATH%java.exe %JAVA_OPTS% -cp "..\lib\*;" step.grid.agent.Agent -config="..\conf\AgentConf.json"
+"%JAVA_PATH%java.exe" %JAVA_OPTS% -cp "..\lib\*;" step.grid.agent.Agent -config="..\conf\AgentConf.json"
 
