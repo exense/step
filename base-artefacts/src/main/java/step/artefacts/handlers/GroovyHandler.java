c8a84f6dbe93e06a1c11db3dbaef1ce1ac4cb4e5
Jérôme Comte <jcomte@denkbar.io>
2016-12-11 20:40:02 +0100

    Adding artefact Groovy

---
+++ base-artefacts/src/main/java/step/artefacts/handlers/GroovyHandler.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * (C) Copyright 2016 Jerome Comte and Dorian Cransac
+ *  
+ * This file is part of STEP
+ *  
+ * STEP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * STEP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Affero General Public License
+ * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package step.artefacts.handlers;
+
+import java.util.Map;
+
+import step.artefacts.Groovy;
+import step.core.artefacts.handlers.ArtefactHandler;
+import step.core.artefacts.reports.ReportNode;
+import step.core.artefacts.reports.ReportNodeStatus;
+import step.core.execution.ExecutionContext;
+import step.expressions.ExpressionHandler;
+
+public class GroovyHandler extends ArtefactHandler<Groovy, ReportNode> {
+	
+	@Override
+	protected void createReportSkeleton_(ReportNode parentNode, Groovy testArtefact) {
+
+	}
+
+	@Override
+	protected void execute_(ReportNode node, Groovy testArtefact) {
+		ExpressionHandler expressionHandler = new ExpressionHandler();
+		Map<String, Object> bindings = ExecutionContext.getCurrentContext().getVariablesManager().getAllVariables();
+		
+		expressionHandler.evaluateGroovyExpression(testArtefact.getExpression(), bindings);
+		node.setStatus(ReportNodeStatus.PASSED);
+	}
+
+	@Override
+	public ReportNode createReportNode_(ReportNode parentNode, Groovy testArtefact) {
+		return new ReportNode();
+	}
+}
