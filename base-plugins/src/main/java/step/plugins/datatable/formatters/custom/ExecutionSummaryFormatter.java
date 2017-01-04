3ceab18979d555b5c2a43419bd7aa9518f6530c2
Jérôme Comte <jcomte@denkbar.io>
2017-01-04 13:11:06 +0100

    Enabling real time monitoring of ExecutionSummary in ExecutionList

--- base-plugins/src/main/java/step/plugins/datatable/formatters/custom/ExecutionSummaryFormatter.java
+++ base-plugins/src/main/java/step/plugins/datatable/formatters/custom/ExecutionSummaryFormatter.java
@@ -6,26 +6,26 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import step.core.GlobalContext;
-import step.core.execution.model.Execution;
 import step.plugins.datatable.formatters.Formatter;
-import step.plugins.views.ViewModelAccessor;
+import step.plugins.views.ViewPlugin;
 import step.plugins.views.functions.ReportNodeStatusDistribution;
 
 public class ExecutionSummaryFormatter implements Formatter {
 	
 	ObjectMapper mapper = new ObjectMapper();
-	
-	ViewModelAccessor viewModelAccessor;
+		
+	ViewPlugin viewPlugin;
 
 	public ExecutionSummaryFormatter(GlobalContext context) {
 		super();
-		this.viewModelAccessor = new ViewModelAccessor(context.getMongoClient(), context.getMongoDatabase());;
+		this.viewPlugin = (ViewPlugin) context.get(ViewPlugin.VIEW_PLUGIN_KEY);
+		
 	}
 
 	@Override
 	public String format(Object value, Document row) {
 		String eid = row.get("_id").toString();
-		ReportNodeStatusDistribution distribution = viewModelAccessor.get("statusDistributionForFunctionCalls", eid, ReportNodeStatusDistribution.class);
+		ReportNodeStatusDistribution distribution = (ReportNodeStatusDistribution) viewPlugin.query("statusDistributionForFunctionCalls", eid);
 		if(distribution!=null) {
 			try {
 				return mapper.writeValueAsString(distribution);
