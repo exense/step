ca54fc9d969b7b796ad2efe8a14809cade3a0849
dcransac <dcransac@gmail.com>
2017-01-11 14:10:11 +0100

    update rtm war (m versions have to be bumped) + improved plugin url

--- rtm-plugin/src/main/java/step/rtm/RtmPluginServices.java
+++ rtm-plugin/src/main/java/step/rtm/RtmPluginServices.java
@@ -1,5 +1,8 @@
 package step.rtm;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -24,12 +27,42 @@ public class RtmPluginServices {
 	@Produces(MediaType.APPLICATION_JSON)
 	public RTMLink getRtmLink(@PathParam("id") String executionID) {
 		RTMLink link = new RTMLink();
-		link.link = getAggregateViewByEid(executionID);
+		try {
+			link.link = getAggregateViewByEid(executionID);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		return link;
 	}	
 	
-	private String getAggregateViewByEid(String eid) {
-		return "rtm/#Aggregate/select/%7B%22guiParams%22%3A%7B%22postControllerView%22%3A%5B%7B%22filters%22%3A%5B%7B%22type%22%3A%22text%22%2C%22key%22%3A%22eid%22%2C%22value%22%3A%22"+
-				eid+"%22%2C%22regex%22%3Afalse%7D%5D%7D%5D%2C%22measurementListView%22%3A%7B%22nextFactor%22%3A0%2C%22tableMetricChoice%22%3A%5B%22begin%22%2C%22name%22%2C%22value%22%5D%7D%2C%22aggregateSPView%22%3A%7B%22sessionId%22%3A%22defaultSid%22%2C%22granularity%22%3A%22900000%22%2C%22groupby%22%3A%22name%22%7D%2C%22aggregateGraphView%22%3A%7B%22chartMetricChoice%22%3A%22avg%22%7D%2C%22aggregateTableView%22%3A%7B%22checkedAggTableMetrics%22%3A%5B%22begin%22%2C%22avg%22%2C%22cnt%22%5D%2C%22isSwitchedOn%22%3A%22false%22%7D%7D%7D/1471591887164";
+	private String getAggregateViewByEid(String eid) throws UnsupportedEncodingException {
+		String quickfix = System.getProperty("rtmDefaultGranularity","120000");
+		return "rtm/#Aggregate/select/"+URLEncoder.encode(
+				"{ \"guiParams\":"
+				    + "{ \"postControllerView\":"
+				        + " [ { \"filters\":"
+				                + " [  { \"type\":\"text\","
+				                      + "\"key\":\"eid\","
+				                      + "\"value\":\""+eid+"\",\"regex\":false"
+		                        + " }  ]"
+		                + " } ],"
+	                + "\"measurementListView\":"
+	                	+ " { \"nextFactor\":0,"
+	                	+    "\"tableMetricChoice\":"
+	                			+ "[\"begin\",\"name\",\"value\"]"
+            			+ " },"
+        			+ "\"aggregateSPView\":"
+        				+ " { \"sessionId\":\"defaultSid\","
+        				+    "\"granularity\":\""+quickfix+"\","
+        				+    "\"groupby\":\"name\" },"
+    				+ "\"aggregateGraphView\":"
+        				+ " { \"chartMetricChoice\":\"avg\"},"
+    				+ "\"aggregateTableView\":"
+    					+ " { \"checkedAggTableMetrics\":"
+    						+ " [ \"begin\", \"avg\", \"cnt\"]"
+    						+ ",\"isSwitchedOn\":\"false\" }"
+					+ "}"
+				+ "}"
+				, "UTF-8").replace("+", "%20");
 	}
 }
