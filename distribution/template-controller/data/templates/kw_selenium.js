a1d5248e4eb4760a3113d8e3febc6039e03fe394
Jérôme Comte <jcomte@denkbar.io>
2017-01-25 14:10:29 +0100

    Updating selenium script template

--- distribution/template-controller/data/templates/kw_selenium.js
+++ distribution/template-controller/data/templates/kw_selenium.js
@@ -1,3 +1,15 @@
-driver = session.get("driver").getWebDriver()
+// Use the following if you're starting a new driver:
+var FirefoxDriver = Java.type("org.openqa.selenium.firefox.FirefoxDriver");
+var DriverWrapper = Java.type("step.script.selenium.DriverWrapper")
 
-//driver.get(input.getString("url"))
+java.lang.System.getProperties().put("webdriver.gecko.driver", "C:/Users/jcomte/Programs/geckodriver-v0.10.0-win64/geckodriver.exe")
+
+var driver = new FirefoxDriver()
+session.put("driver",new DriverWrapper(driver))
+
+// Use the following if you're reusing a driver
+
+// driver = session.get("driver").getWebDriver()
+
+// Example:
+// driver.navigate(driver.get(input.getString("url")))
