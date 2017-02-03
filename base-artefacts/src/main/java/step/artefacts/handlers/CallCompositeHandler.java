883789e92c7261ed72b8a273785203c5f644ad79
Jérôme Comte <jcomte@denkbar.io>
2017-02-03 18:09:07 +0100

    Implementing PoC for composite control

---
+++ base-artefacts/src/main/java/step/artefacts/handlers/CallCompositeHandler.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * (C) Copyright 2016 Jerome Comte and Dorian Cransac
+ *  
+ * This file is part of STEP
+ *  
+ * STEP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * STEP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Affero General Public License
+ * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package step.artefacts.handlers;
+
+import step.artefacts.CallCompositeControl;
+import step.core.artefacts.AbstractArtefact;
+import step.core.artefacts.handlers.ArtefactHandler;
+import step.core.artefacts.reports.ReportNode;
+
+public class CallCompositeHandler extends ArtefactHandler<CallCompositeControl, ReportNode> {
+
+	@Override
+	protected void createReportSkeleton_(ReportNode parentNode,	CallCompositeControl testArtefact) {
+		context.getVariablesManager().putVariable(parentNode, "#placeholder", testArtefact);
+
+		
+		AbstractArtefact a = context.getGlobalContext().getArtefactAccessor().get(testArtefact.getArtefactId());
+		delegateCreateReportSkeleton(a, parentNode);
+	}
+
+	@Override
+	protected void execute_(ReportNode node, CallCompositeControl testArtefact) {
+		context.getVariablesManager().putVariable(node, "#placeholder", testArtefact);
+		
+		AbstractArtefact a = context.getGlobalContext().getArtefactAccessor().get(testArtefact.getArtefactId());
+		delegateExecute(a, node);
+	}
+
+	@Override
+	public ReportNode createReportNode_(ReportNode parentNode, CallCompositeControl testArtefact) {
+		return new ReportNode();
+	}
+
+}
