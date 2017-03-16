e5677ce278758dee46fd802eef7b24f951eea8a3
Jérôme Comte <jcomte@denkbar.io>
2017-03-16 09:32:15 +0100

    Fixing demo keywords

--- distribution/template-controller/data/scripts/demo-selenium-keyword/src/main/java/io/denkbar/step/examples/selenium/SeleniumKeywordExample.java
+++ distribution/template-controller/data/scripts/demo-selenium-keyword/src/main/java/io/denkbar/step/examples/selenium/SeleniumKeywordExample.java
@@ -1,9 +1,7 @@
 package io.denkbar.step.examples.selenium;
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -22,25 +20,31 @@ public class SeleniumKeywordExample extends AbstractScript {
 	@Function
 	public void Google_Search() throws Exception {
 		if(input.containsKey("search")) {
-			System.setProperty("webdriver.chrome.driver", "C:/Users/jcomte/Programs/chromedriver_win32_2.27/chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			
-			driver.get("http://www.google.com");
-			
-			WebElement searchInput = driver.findElement(By.id("lst-ib"));
-			
-			String searchString = input.getString("search");
-			searchInput.sendKeys(searchString);			
-			
-			WebElement searchButton = driver.findElement(By.xpath("//button[@name='btnG']"));
-			searchButton.click();
-			
-			WebElement resultCountDiv = driver.findElement(By.xpath("//div/nobr"));
-			
-			List<WebElement> resultHeaders = driver.findElements(By.xpath("//h3[@class='r']"));
-			for(WebElement result:resultHeaders) {
-				output.add(result.getText(), result.findElement(By.xpath("..//cite")).getText());
+			File chromedriverExe = new File("../ext/bin/chromedriver/chromedriver.exe");
+			if(chromedriverExe.exists()) {
+				System.setProperty("webdriver.chrome.driver", chromedriverExe.getAbsolutePath());
+				WebDriver driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				
+				driver.get("http://www.google.com");
+				
+				WebElement searchInput = driver.findElement(By.id("lst-ib"));
+				
+				String searchString = input.getString("search");
+				searchInput.sendKeys(searchString);			
+				
+				WebElement searchButton = driver.findElement(By.xpath("//button[@name='btnG']"));
+				searchButton.click();
+				
+				WebElement resultCountDiv = driver.findElement(By.xpath("//div/nobr"));
+				
+				List<WebElement> resultHeaders = driver.findElements(By.xpath("//h3[@class='r']"));
+				for(WebElement result:resultHeaders) {
+					output.add(result.getText(), result.findElement(By.xpath("..//cite")).getText());
+				}
+				
+			} else {
+				output.setError("Unable to find chromedriver.exe in '"+chromedriverExe.getParent() +"'.");
 			}
 			
 		} else {
