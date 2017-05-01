16716ca9d1c210352d34e4b7d7e1fb0349aadb72
Jérôme Comte <jcomte@denkbar.io>
2017-05-01 12:30:02 +0200

    Implementing artefact form for FunctionGroup

--- functions/src/main/java/step/artefacts/FunctionGroup.java
+++ functions/src/main/java/step/artefacts/FunctionGroup.java
@@ -18,32 +18,10 @@
  *******************************************************************************/
 package step.artefacts;
 
-import java.util.Map;
-
 import step.artefacts.handlers.FunctionGroupHandler;
-import step.core.artefacts.AbstractArtefact;
 import step.core.artefacts.Artefact;
 
 @Artefact(handler = FunctionGroupHandler.class)
-public class FunctionGroup extends AbstractArtefact {
+public class FunctionGroup extends TokenSelector {
 
-	private Map<String, String> selectionCriteria;
-	
-	private Map<String, String> attributes;
-
-	public Map<String, String> getSelectionCriteria() {
-		return selectionCriteria;
-	}
-
-	public void setSelectionCriteria(Map<String, String> selectionCriteria) {
-		this.selectionCriteria = selectionCriteria;
-	}
-
-	public Map<String, String> getAttributes() {
-		return attributes;
-	}
-
-	public void setAttributes(Map<String, String> attributes) {
-		this.attributes = attributes;
-	}
 }
