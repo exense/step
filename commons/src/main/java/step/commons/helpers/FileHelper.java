4819413fdddf1d18d891933d26ab11865d19ddd9
Jérôme Comte <jcomte@denkbar.io>
2017-06-05 23:04:41 +0200

    Readding method deleteFolder ind FileHelper

--- commons/src/main/java/step/commons/helpers/FileHelper.java
+++ commons/src/main/java/step/commons/helpers/FileHelper.java
@@ -41,6 +41,20 @@ import step.commons.conf.Configuration;
 
 public class FileHelper {
 
+	public static void deleteFolder(File folder) {
+		File[] files = folder.listFiles();
+		if (files != null) {
+			for (File f : files) {
+				if (f.isDirectory()) {
+					deleteFolder(f);
+				} else {
+					f.delete();
+				}
+			}
+		}
+		folder.delete();
+	}
+	
 	public static void deleteFolderOnExit(File folder) {
 		File[] files = folder.listFiles();
 		if (files != null) {
