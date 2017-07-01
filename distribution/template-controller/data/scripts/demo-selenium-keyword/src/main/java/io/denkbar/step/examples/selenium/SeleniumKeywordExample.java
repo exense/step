4ddb8b4eb5058226f7ed7a3725d65f081fd13cd1
Jérôme Comte <jcomte@denkbar.io>
2017-07-01 18:07:47 +0200

    Improving SeleniumKeyword examples

--- distribution/template-controller/data/scripts/demo-selenium-keyword/src/main/java/io/denkbar/step/examples/selenium/SeleniumKeywordExample.java
+++ distribution/template-controller/data/scripts/demo-selenium-keyword/src/main/java/io/denkbar/step/examples/selenium/SeleniumKeywordExample.java
@@ -2,11 +2,14 @@ package io.denkbar.step.examples.selenium;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -36,49 +39,60 @@ public class SeleniumKeywordExample extends AbstractScript {
 		}	
 	}
 	
-	@Function
+	@Function(name="Open Chrome")
+	public void openChrome() throws Exception {
+		File chromedriverExe = new File(properties.get("chromedriver"));
+		if(chromedriverExe.exists()) {
+			System.setProperty("webdriver.chrome.driver", chromedriverExe.getAbsolutePath());
+			final WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			session.put(new DriverWrapper(driver));
+		} else {
+			output.setError("Unable to find chromedriver.exe in '"+chromedriverExe.getParent() +"'.");
+		}
+	}
+	
+	@Function(name="Search in google")
 	public void Google_Search() throws Exception {
 		if(input.containsKey("search")) {
-			File chromedriverExe = new File("../ext/bin/chromedriver/chromedriver.exe");
-			if(chromedriverExe.exists()) {
-				System.setProperty("webdriver.chrome.driver", chromedriverExe.getAbsolutePath());
-				final WebDriver driver = new ChromeDriver();
-				
-				session.put("driver", new DriverWrapper(driver));
-				
-				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-				
-				driver.get("http://www.google.com");
-				
-				WebElement searchInput = driver.findElement(By.id("lst-ib"));
-				
-				String searchString = input.getString("search");
-				searchInput.sendKeys(searchString);			
-				
-				WebElement searchButton = driver.findElement(By.xpath("//button[@name='btnG']"));
-				searchButton.click();
-				
-				WebElement resultCountDiv = driver.findElement(By.xpath("//div/nobr"));
-				
-				List<WebElement> resultHeaders = driver.findElements(By.xpath("//h3[@class='r']"));
-				for(WebElement result:resultHeaders) {
-					output.add(result.getText(), result.findElement(By.xpath("..//cite")).getText());
-				}
-				
-			} else {
-				output.setError("Unable to find chromedriver.exe in '"+chromedriverExe.getParent() +"'.");
-			}
+			final WebDriver driver = session.get(DriverWrapper.class).driver;
 			
+			driver.get("http://www.google.com");
+			
+			WebElement searchInput = driver.findElement(By.id("lst-ib"));
+			
+			String searchString = input.getString("search");
+			searchInput.sendKeys(searchString);			
+			
+			WebElement searchButton = driver.findElement(By.xpath("//button[@name='btnG']"));
+			searchButton.click();
+			
+			WebElement resultCountDiv = driver.findElement(By.xpath("//div/nobr"));
+			
+			List<WebElement> resultHeaders = driver.findElements(By.xpath("//h3[@class='r']"));
+			for(WebElement result:resultHeaders) {
+				output.add(result.getText(), result.findElement(By.xpath("..//cite")).getText());
+			}
 		} else {
 			output.setError("Input parameter 'search' not defined");
 		}
 	}
 	
-	ScriptContext ctx = ScriptRunner.getExecutionContext();
+	ScriptContext ctx;
+	
+	@Before
+	public void setUp() {
+		Map<String, String> properties = new HashMap<>();
+		properties.put("chromedriver", "../ext/bin/chromedriver/chromedriver.exe");
+		ctx = ScriptRunner.getExecutionContext(properties);
+	}
 	
 	@Test
 	public void test() {
-	    OutputMessage result = ctx.run(SeleniumKeywordExample.class,"Google_Search","{ \"search\" : \"denkbar step\" }");
+	    OutputMessage result;
+	    result = ctx.run(SeleniumKeywordExample.class,"Open Chrome","{ \"search\" : \"denkbar step\" }");
+	    result = ctx.run(SeleniumKeywordExample.class,"Search in google","{ \"search\" : \"denkbar step\" }");
+	    result = ctx.run(SeleniumKeywordExample.class,"Search in google","{ \"search\" : \"denkbar djigger\" }");
 	    Assert.assertNull(result.getError());
 	    result.getPayload();
 	}
