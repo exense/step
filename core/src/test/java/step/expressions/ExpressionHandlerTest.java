95aae27caf4fd4920181fa03968219ebafd3c161
Jérôme Comte <jcomte@denkbar.io>
2017-08-17 10:26:09 +0200

    Fixing scriptBaseClass issue in GroovyPool + implementing JUnit Tests for ExpressionHandler

---
+++ core/src/test/java/step/expressions/ExpressionHandlerTest.java
@@ -0,0 +1,45 @@
+package step.expressions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ExpressionHandlerTest {
+
+	@Test
+	public void testDefault() {
+		ExpressionHandler e = new ExpressionHandler();
+		Object o = e.evaluateGroovyExpression("1+1", null);
+		Assert.assertEquals(2,o);
+	}
+	
+	@Test
+	public void testBindings() {
+		ExpressionHandler e = new ExpressionHandler("step.expressions.GroovyFunctions");
+		Map<String, Object> b = new HashMap<>();
+		b.put("test", "value");
+		Object o = e.evaluateGroovyExpression("test", b);
+		Assert.assertEquals("value", o.toString());
+	}
+	
+	@Test
+	public void testScriptBaseClass() {
+		ExpressionHandler e = new ExpressionHandler("step.expressions.GroovyFunctions");
+		Object o = e.evaluateGroovyExpression("yyyyMMdd", null);
+		SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd");
+		Assert.assertEquals(f.format(new Date()), o.toString());
+	}
+
+	@Test
+	public void testTemplates() {
+		ExpressionHandler e = new ExpressionHandler("step.expressions.GroovyFunctions");
+		Object o = e.evaluateGroovyExpression("\"Today is ${yyyyMMdd}\"", null);
+		SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd");
+		Assert.assertEquals("Today is " +f.format(new Date()), o.toString());
+	}
+}
