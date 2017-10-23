5bf4faa0509bac51b69c3b636946db26719e341a
Jérôme Comte <jcomte@denkbar.io>
2017-10-23 10:25:02 +0200

    Adding method requestFileVersion to FileManager

--- grid/src/main/java/step/grid/filemanager/FileManagerClient.java
+++ grid/src/main/java/step/grid/filemanager/FileManagerClient.java
@@ -5,5 +5,40 @@ import java.io.File;
 public interface FileManagerClient {
 
 	File requestFile(String uid, long lastModified);
+	
+	FileVersion requestFileVersion(String uid, long lastModified);
+	
+	static class FileVersion {
+		
+		File file;
+		
+		boolean modified;
+		
+		String fileId;
+
+		public String getFileId() {
+			return fileId;
+		}
+
+		public void setFileId(String fileId) {
+			this.fileId = fileId;
+		}
+
+		public File getFile() {
+			return file;
+		}
+
+		public void setFile(File file) {
+			this.file = file;
+		}
+
+		public boolean isModified() {
+			return modified;
+		}
+
+		public void setModified(boolean modified) {
+			this.modified = modified;
+		}
+	}
 
 }
