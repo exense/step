f8183b49053b14f1ec980df471cbf95bb1927ce1
Jérôme Comte <jerome.comte@exense.ch>
2018-02-15 20:27:43 +0100

    Returning String instead of SimpleStringMap by FlatFileReader

--- base-artefacts/src/main/java/step/datapool/file/FlatFileReaderDataPool.java
+++ base-artefacts/src/main/java/step/datapool/file/FlatFileReaderDataPool.java
@@ -1,10 +1,5 @@
 package step.datapool.file;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import step.core.variables.SimpleStringMap;
-
 public class FlatFileReaderDataPool extends FileReaderDataPool {
 
 	public FlatFileReaderDataPool(FileDataPool configuration) {
@@ -13,46 +8,9 @@ public class FlatFileReaderDataPool extends FileReaderDataPool {
 
 	@Override
 	public Object postProcess(String line) {
-		return new FlatLineRowWrapper(line);
+		return line;
 	}
-
-	public class FlatLineRowWrapper extends SimpleStringMap {
-
-		String line;
-		
-		public FlatLineRowWrapper(String line) {
-			super();
-			this.line = line;
-		}
-
-		@Override
-		public String get(String key) {
-			return line;
-		}
-
-		@Override
-		public int size() {
-			return 1;
-		}
-
-		@Override
-		public boolean isEmpty() {
-			return false;
-		}
-
-		@Override
-		public String put(String key, String value) {
-			throw new RuntimeException("Put not supported on flat file data pool");
-		}
-
-		@Override
-		public Set<String> keySet() {
-			Set<String> keySet = new HashSet<>();
-			keySet.add("row");
-			return keySet;
-		}
-	}
-
+	
 	@Override
 	public void doFirst_() {}
 	
