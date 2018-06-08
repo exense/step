8c99fa058f0d3b2cdc9b62cbd033ca8f423ba19a
Jérôme Comte <jerome.comte@exense.ch>
2018-06-08 18:22:52 +0200

    Fixing critical application loading issue in IE

--- execution-controller/src/main/resources/webapp/js/bootstrap.js
+++ execution-controller/src/main/resources/webapp/js/bootstrap.js
@@ -1,28 +1,35 @@
-$.get("rest/app/plugins",function(plugins) {
-  var scripts = [];
-  var angularModules = [];
-  _.each(plugins,function(plugin) {
-    angularModules = angularModules.concat(plugin.angularModules);
-    scripts = scripts.concat(plugin.scripts);   
-  })
+console.log("Initializing bootstrapper")
+var step = {
+    bootstrap: function() {
+      $.get("rest/app/plugins",function(plugins) {
+        console.log("Loading plugins")
+        var scripts = [];
+        var angularModules = [];
+        _.each(plugins,function(plugin) {
+          angularModules = angularModules.concat(plugin.angularModules);
+          scripts = scripts.concat(plugin.scripts);   
+        })
 
-  var deferred = new $.Deferred(), pipe = deferred;
+        var deferred = new $.Deferred(), pipe = deferred;
 
-  $.each(scripts, function(i, val) {
-    pipe = pipe.pipe(function() {
-      return loadScript(val);
-    });
-  });
+        $.each(scripts, function(i, val) {
+          pipe = pipe.pipe(function() {
+            return loadScript(val);
+          });
+        });
 
-  pipe.pipe(function() {
-    _.each(angularModules, function(module) {
-      angular.module('tecAdminApp').requires.push(module);      
-    })
-    angular.bootstrap(document,['tecAdminApp'])
-  })
-  
-  deferred.resolve();
-})
+        pipe.pipe(function() {
+          _.each(angularModules, function(module) {
+            angular.module('tecAdminApp').requires.push(module);      
+          })
+          console.log("Bootstrapping angular");
+          angular.bootstrap(document,['tecAdminApp'])
+        })
+        
+        deferred.resolve();
+      })      
+    }
+}
 
 function loadScript(filename){
     var d = $.Deferred()
