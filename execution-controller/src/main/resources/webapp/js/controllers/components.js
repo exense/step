ecd22033f2ffb18b862092a829f9c9014233679a
Jérôme Comte <jerome.comte@exense.ch>
2018-09-13 19:37:06 +0200

    Implementing new UI component fileUploadField

--- execution-controller/src/main/resources/webapp/js/controllers/components.js
+++ execution-controller/src/main/resources/webapp/js/controllers/components.js
@@ -12,3 +12,85 @@ angular.module('components',['step'])
     }
   };
 })
+
+.directive('fileUploadField', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      file: '=',
+      saveButton: '@',
+      saveButtonLabel: '@',
+      onFileSelected: '&'
+    },
+    controller: function($scope,$http,$timeout,Upload) {
+      
+      if(!$scope.saveButtonLabel) {
+        $scope.saveButtonLabel="Save"
+      }
+      
+      $scope.upload = function (file) {
+        if(file) {
+          Upload.upload({
+            url: 'rest/files',
+            data: {file: file}
+          }).then(function (resp) {
+            attachmentId = resp.data.attachmentId; 
+            $scope.file = "attachment:"+attachmentId;
+            notifyFileSelection();
+          }, function (resp) {
+            console.log('Error status: ' + resp.status);
+          }, function (evt) {
+            $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+          });          
+        }
+      };
+      $scope.isAttachment = function() {
+        return typeof $scope.file == 'string' && $scope.file.indexOf('attachment:')==0;
+      }
+      $scope.getAttachmentId = function() {
+        return $scope.file.replace("attachment:","");
+      }
+      
+      $scope.attachmentFilename = "";
+      $scope.$watch('file',function(newValue) {
+        $scope.model.filePath = newValue;
+        if(newValue && $scope.isAttachment()) {
+          $http.get("rest/files/"+$scope.getAttachmentId()+"/name").then(
+            function(response) {
+              $scope.attachmentFilename = response.data;
+            });
+        }
+      })
+      
+      $scope.clear = function() {
+        $http.delete("rest/files/"+$scope.getAttachmentId());
+        $scope.file = '';
+      }
+      
+      // Using model.filePath and not filePath directly to avoid two-way binding issues caused by the ng-if in the template
+      // See https://stackoverflow.com/questions/12618342/ng-model-does-not-update-controller-value
+      $scope.model = {filePath: $scope.file};
+      
+      $scope.blur = function() {
+        if(!$scope.showSaveButton()) {
+          $scope.save();
+        }
+       }
+      
+      $scope.showSaveButton = function() {
+        return $scope.saveButton && $scope.saveButton=='true';
+      }
+      
+      $scope.save = function() {
+       $scope.file = $scope.model.filePath;
+       notifyFileSelection();
+      }
+      
+      function notifyFileSelection() {
+        $timeout(function() {
+          $scope.onFileSelected(); 
+        })
+      }
+    },
+    templateUrl: 'partials/components/fileUploadField.html'}
+})
