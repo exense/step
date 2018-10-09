ce2aa4e825b61e74c81bcbcdbab785bd11414bfa
Jérôme Comte <jerome.comte@exense.ch>
2018-10-09 20:59:21 +0200

    Extending st-table component
    - adding support for in-memory tables
    - fixing memory leak on refresh (adding scope tracking)
    - implementing table state persistence

--- execution-controller/src/main/resources/webapp/js/ntables.js
+++ execution-controller/src/main/resources/webapp/js/ntables.js
@@ -17,26 +17,62 @@
  * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 angular.module('tables', ['export','dataTable'])
-.directive('stTable', function($compile, $http, Preferences) {
+.directive('stTable', function($compile, $http, Preferences, stateStorage) {
   return {
     restrict : 'E',
     scope : {
-      handle: '='
+      uid: '=',
+      handle: '=',
+      data: '=',
+      collection: '=',
+      persistState: '='
     },
     transclude : {
       'actions' : '?actions',
       'columns' : '?columns'
     },
     link : function(scope, element, attr, tabsCtrl, linker) {
+      var serverSide = scope.collection?true:false;
+      
+      scope.scopesTracker = new ScopeTracker();
+      scope.headerScopesTracker = new ScopeTracker();
+      scope.$on('$destroy', function() {
+        scope.scopesTracker.destroy();
+        scope.headerScopesTracker.destroy();
+        if(scope.table) {
+          scope.table.destroy();
+        }
+      });
+      
       var tableElement = angular.element(element).find('table');
 
       var tableOptions = {}
-      tableOptions.pageLength = Preferences.get("tables_itemsperpage", 10);
-      tableOptions.processing = false;
-      tableOptions.serverSide = true;
+      tableOptions.pageLength = parseInt(Preferences.get("tables_itemsperpage", 10));
       tableOptions.dom = 'lrtip';
-      tableOptions.sProcessing = '';
+      tableOptions.fnDrawCallback = function() {
+        scope.scopesTracker.newCycle();
+      };
 
+      if (scope.persistState) {
+        if (scope.uid) {
+          var uid = scope.uid;
+          tableOptions.stateSave = true;
+          tableOptions.stateSaveCallback = function(settings, data) {
+            var state = stateStorage.get(scope, uid);
+            if (!state) {
+              state = {};
+            }
+            state.tableState = data;
+            stateStorage.store(scope, state, uid);
+          };
+          tableOptions.stateLoadCallback = function(settings) {
+            var state = stateStorage.get(scope, uid);
+            return (state && state.tableState) ? state.tableState : null;
+          }
+        } else {
+          console.error("Unable to persist table state if the table uid isn't specified. Please set the attribute 'uid'")
+        }
+      }
 
       // Table columns
       var columns = linker(function() {}, null, 'columns');
@@ -50,25 +86,40 @@ angular.module('tables', ['export','dataTable'])
         
         var colDef = {
           "title" : header,
-          "name" : name
+          "name" : name,
+          "data": name
         };
 
         colDef.createdCell = function(td, cellData, rowData, row, col) {
           var rowScope = scope.$new(false, scope.$parent);
-          rowScope.row = JSON.parse(rowData[0]);
-          var content = $compile(cell)(rowScope);
+          if(serverSide) {
+            rowScope.row = JSON.parse(rowData[0]);
+          } else {
+            rowScope.row = rowData;
+          }
+          
+          if(!colDef.compiledCell) {
+            colDef.compiledCell = $compile(cell);
+          }
+          
+          var content = colDef.compiledCell(rowScope, function(){});
           angular.element(td).empty();
           angular.element(td).append(content);
-          rowScope.$digest();
+          if(serverSide) {
+            rowScope.$digest();
+          }
+          scope.scopesTracker.track(rowScope);
         };
         
         function createHeaderRenderer(headerHtml) {
-          return function(element, handle) {
+          return function(element, column, handle) {
             var headerScope = scope.$new(false, scope.$parent);
+            headerScope.initialValue = column.search();
             headerScope.search = function(expression){handle.search(name,expression)};
             var content = $compile(headerHtml)(headerScope);
             element.empty();
             element.append(content);
+            scope.headerScopesTracker.track(headerScope);
           }
         }
         colDef.headerRenderer = createHeaderRenderer(header);
@@ -78,14 +129,28 @@ angular.module('tables', ['export','dataTable'])
       })
       tableOptions.columns = dtColumns;
 
-      
-      var query = 'rest/table/' + attr.collection + '/data';
-      tableOptions.ajax = {
-        'url' : query,
-        'type' : 'POST'
+      if(serverSide) {
+        var query = 'rest/table/' + scope.collection + '/data';
+        tableOptions.ajax = {
+            'url' : query,
+            'type' : 'POST'
+        }
+        tableOptions.processing = false;
+        tableOptions.serverSide = true;
+        tableOptions.sProcessing = '';
+      } else {
+        scope.$watchCollection('data', function(value) {
+          if(scope.table) {
+            scope.table.clear();
+            if (value && value.length > 0) {
+              scope.table.rows.add(value).draw(false);
+            }
+          }
+        }) 
       }
       
       var table = tableElement.DataTable(tableOptions);
+      scope.table = table;
       
       // Table actions
       var tableActions = linker(function() {}, null, 'actions');
@@ -99,19 +164,20 @@ angular.module('tables', ['export','dataTable'])
       }
       angular.element('<div class="pull-right"></div>').append(tableActions).appendTo(cmdDiv);
 
-      if(scope.handle) {
-        scope.handle.reload = function() {
-          table.ajax.reload(null, false);
-        }
-        scope.handle.search = function(columnName, searchExpression) {
-          var column = table.column(columnName+':name');
-          column.search(searchExpression,true,false).draw();
-        }
+      if(!scope.handle) {
+        scope.handle = {};
+      }
+      scope.handle.reload = function() {
+        table.ajax.reload(null, false);
+      }
+      scope.handle.search = function(columnName, searchExpression) {
+        var column = table.column(columnName+':name');
+        column.search(searchExpression,true,false).draw();
       }
       
       // render first header
       table.columns().indexes().flatten().each(function(i) {
-        table.settings()[0].aoColumns[i].headerRenderer(angular.element(table.column(i).header()),scope.handle);
+        table.settings()[0].aoColumns[i].headerRenderer(angular.element(table.column(i).header()),table.column(i),scope.handle);
       })
       
       // render second header
@@ -123,9 +189,10 @@ angular.module('tables', ['export','dataTable'])
         var thIdx = $('th',tableElement.find('thead tr[role="row"]')).index(table.column(i).header());
         if(thIdx>=0) {
           var secondHeader = $('th',tableElement.find('thead tr.searchheader')).eq(thIdx);
-          table.settings()[0].aoColumns[i].secondHeaderRenderer(secondHeader,scope.handle);
+          table.settings()[0].aoColumns[i].secondHeaderRenderer(secondHeader,table.column(i),scope.handle);
         }
       });
+      
 
     },
     templateUrl : 'partials/datatable.html'
