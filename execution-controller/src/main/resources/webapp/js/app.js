a2488a501d2153dc2eea2f484f300e8d71387d4f
Jérôme Comte <jerome.comte@exense.ch>
2018-10-19 11:21:12 +0200

    Fixing rendering issue of attachment-preview in IE11

--- execution-controller/src/main/resources/webapp/js/app.js
+++ execution-controller/src/main/resources/webapp/js/app.js
@@ -437,3 +437,23 @@ angular.module('step',['ngStorage','ngCookies'])
         return $q.reject(response);
     };
 })
+
+// The following functions are missing in IE11
+
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+    var subjectString = this.toString();
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+      position = subjectString.length;
+    }
+    position -= searchString.length;
+    var lastIndex = subjectString.indexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    return this.substr(position || 0, searchString.length) === searchString;
+  };
+}
