91a4e2debfd0faf2b618f31e8fd0e3699a9c0cec
Jérôme Comte <jerome.comte@exense.ch>
2018-10-25 11:09:46 +0200

    Adding javadoc comments

--- core/src/main/java/step/core/artefacts/ArtefactAccessor.java
+++ core/src/main/java/step/core/artefacts/ArtefactAccessor.java
@@ -13,8 +13,21 @@ public interface ArtefactAccessor extends CRUDAccessor<AbstractArtefact> {
 	<T extends AbstractArtefact> T createWorkArtefact(Class<T> artefactClass, AbstractArtefact parentArtefact,
 			String name, boolean copyChildren);
 
+	/**
+	 * Find an artefact by attributes
+	 * 
+	 * @param attributes the map of mandatory attributes 
+	 * @return the artefact
+	 */
 	AbstractArtefact findByAttributes(Map<String, String> attributes);
 
+	/**
+	 * Find a root artefact by attributes. A root artefact is an artefact that is listed in the "Plans" view.
+	 * Non root artefacts are the nodes of a Plan
+	 * 
+	 * @param attributes the map of mandatory attributes 
+	 * @return the artefact
+	 */
 	AbstractArtefact findRootArtefactByAttributes(Map<String, String> attributes);
 
 	AbstractArtefact get(String artefactID);
