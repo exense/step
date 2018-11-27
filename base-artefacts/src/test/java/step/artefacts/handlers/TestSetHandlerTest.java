3309c7c96e752fdc34b37ebb06e8f6830109e79b
Jérôme Comte <jerome.comte@exense.ch>
2018-11-27 14:24:43 +0100

    Fixing status propagation in TestSetHandler

--- base-artefacts/src/test/java/step/artefacts/handlers/TestSetHandlerTest.java
+++ base-artefacts/src/test/java/step/artefacts/handlers/TestSetHandlerTest.java
@@ -20,12 +20,21 @@ package step.artefacts.handlers;
 
 import static junit.framework.Assert.assertEquals;
 
+import java.io.StringWriter;
 import java.util.HashSet;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+import step.artefacts.Check;
 import step.artefacts.CheckArtefact;
 import step.artefacts.TestSet;
 import step.core.artefacts.reports.ReportNode;
 import step.core.artefacts.reports.ReportNodeStatus;
+import step.core.dynamicbeans.DynamicValue;
+import step.core.plans.Plan;
+import step.core.plans.builder.PlanBuilder;
+import step.core.plans.runner.DefaultPlanRunner;
 
 public class TestSetHandlerTest extends AbstractArtefactHandlerTest {
 	
@@ -45,6 +54,56 @@ public class TestSetHandlerTest extends AbstractArtefactHandlerTest {
 		execute(threadIdSet, "1", "DummyName{childID}");
 		
 		assertEquals("Ensure that the TestSetHandler runs the child artefacts sequentially", 1, threadIdSet.size());
+	}
+	
+	@Test
+	public void testStatusReportingFailed() throws Exception {
+		Plan plan = PlanBuilder.create().startBlock(new TestSet()).add(passedCheck()).add(failedCheck()).endBlock().build();
+		DefaultPlanRunner runner = new DefaultPlanRunner();
+		
+		StringWriter writer = new StringWriter();
+		runner.run(plan).printTree(writer);
+		
+		Assert.assertTrue(writer.toString().startsWith("TestSet:"+ReportNodeStatus.FAILED));
+	}
+	
+	@Test
+	public void testStatusReportingPassed() throws Exception {
+		Plan plan = PlanBuilder.create().startBlock(new TestSet()).add(passedCheck()).add(passedCheck()).endBlock().build();
+		DefaultPlanRunner runner = new DefaultPlanRunner();
+		
+		StringWriter writer = new StringWriter();
+		runner.run(plan).printTree(writer);
+		
+		Assert.assertTrue(writer.toString().startsWith("TestSet:"+ReportNodeStatus.PASSED));
+	}
+	
+	@Test
+	public void testStatusReportingError() throws Exception {
+		Plan plan = PlanBuilder.create().startBlock(new TestSet()).add(passedCheck()).add(errorCheck()).add(passedCheck()).endBlock().build();
+		DefaultPlanRunner runner = new DefaultPlanRunner();
+		
+		StringWriter writer = new StringWriter();
+		runner.run(plan).printTree(writer);
+		
+		Assert.assertTrue(writer.toString().startsWith("TestSet:"+ReportNodeStatus.TECHNICAL_ERROR));
+	}
+
+	private Check passedCheck() {
+		Check passedCheck = new Check();
+		passedCheck.setExpression(new DynamicValue<Boolean>(true));
+		return passedCheck;
+	}
+	
+	private Check failedCheck() {
+		Check failedCheck = new Check();
+		failedCheck.setExpression(new DynamicValue<Boolean>(false));
+		return failedCheck;
+	}
+	
+	private Check errorCheck() {
+		Check errorCheck = new Check();
+		return errorCheck;
 	}
 
 	private void execute(HashSet<Long> threadIdSet, String tecExecutionThreads, String childNamePattern) {
