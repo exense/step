c2e4849f13785cbb3b2a22a21c17e42090cdb370
Jérôme Comte <jerome.comte@exense.ch>
2019-01-08 08:37:02 +0100

    Adding javadoc and new constructors to Error and ErrorType

--- step-core-model/src/main/java/step/core/reports/Error.java
+++ step-core-model/src/main/java/step/core/reports/Error.java
@@ -1,17 +1,49 @@
 package step.core.reports;
 
+/**
+ * This class is used to represent all errors that might occur
+ * in STEP either in a control or a function (aka keyword).
+ * 
+ * It can represent a technical error (i.e. an unhandled or unexpected error) 
+ * or a clearly identified error in the SUT
+ *
+ */
 public class Error {
 	
-	ErrorType type = ErrorType.TECHNICAL;
+	protected ErrorType type = ErrorType.TECHNICAL;
 	
-	String layer;
+	protected String layer;
 	
-	String msg;
+	protected String msg;
 	
-	Integer code;
+	protected Integer code;
 	
-	boolean root;
+	protected boolean root;
 
+	/**
+	 * @param type the type of error
+	 * @param message the detailed error message that will be reported 1:1 to the end user)
+	 */
+	public Error(ErrorType type, String message) {
+		this(type, null, message, 0, true);
+	}
+
+	/**
+	 * @param type the type of error 
+	 * @param message the detailed error message (that will be reported 1:1 to the end user)
+	 * @param code a free definable error code to uniquely classify the error that can be used to filter errors in reports
+	 */
+	public Error(ErrorType type, String message, Integer code) {
+		this(type, null, message, code, true);
+	}
+
+	/**
+	 * @param type the type of error 
+	 * @param layer a free text field that describes the layer of the application where the error occurred
+	 * @param msg the detailed error message (that will be reported 1:1 to the end user)
+	 * @param code a free definable error code to uniquely classify the error that can be used to filter errors in reports
+	 * @param root a boolean that defines if this is the root cause of an error or a rethrown error (set to true in 
+	 */
 	public Error(ErrorType type, String layer, String msg, Integer code, boolean root) {
 		super();
 		this.type = type;
@@ -55,6 +87,14 @@ public class Error {
 
 	public void setRoot(boolean root) {
 		this.root = root;
+	}
+
+	public String getLayer() {
+		return layer;
+	}
+
+	public void setLayer(String layer) {
+		this.layer = layer;
 	}
 
 }
--- step-core-model/src/main/java/step/core/reports/ErrorType.java
+++ step-core-model/src/main/java/step/core/reports/ErrorType.java
@@ -2,7 +2,15 @@ package step.core.reports;
 
 public enum ErrorType {
 
+	/**
+	 * Represents a unexpected technical error
+	 * This is reported as TECHNICAL_ERROR
+	 */
 	TECHNICAL,
 	
+	/**
+	 * Represents a clearly identified error (mainly in the SUT)
+	 * This is reported as FAILED
+	 */
 	BUSINESS;
 }
