435d8aaaae19d2d1b961fcc647342046143bf409
Jerome Brongniart <jerome.brongniart@exense.ch>
2019-01-14 14:21:40 +0100

    Put MongoDB log files in the log folder

--- distribution/template-controller/bin/startMongo.bat
+++ distribution/template-controller/bin/startMongo.bat
@@ -26,4 +26,4 @@ rem SET MONGO_PATH="D:\Program Files\MongoDB\Server\3.0\bin"\
 
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
 
-%MONGO_PATH%mongod.exe -dbpath ..\data\mongodb > mongod_%mydate%.log 2>&1
+%MONGO_PATH%mongod.exe -dbpath ..\data\mongodb > ..\log\mongod_%mydate%.log 2>&1
