db0cf33e268962c020b0cd8bc0b35b9dedda6fcc
Jérôme Comte <jerome.comte@exense.ch>
2019-03-13 13:57:13 +0100

    Adding index resourceType to collection resources

--- core/src/main/java/step/resources/ResourceAccessorImpl.java
+++ core/src/main/java/step/resources/ResourceAccessorImpl.java
@@ -1,12 +1,23 @@
 package step.resources;
 
+import org.bson.Document;
+
 import step.core.accessors.AbstractCRUDAccessor;
 import step.core.accessors.MongoClientSession;
 
 public class ResourceAccessorImpl extends AbstractCRUDAccessor<Resource> implements ResourceAccessor {
 
+	com.mongodb.client.MongoCollection<Document> resources_;
+	
 	public ResourceAccessorImpl(MongoClientSession clientSession) {
 		super(clientSession, "resources", Resource.class);
+		resources_ = getMongoCollection("resources");
+		
+		createIndexesIfNeeded();
+	}
+	
+	protected void createIndexesIfNeeded() {
+		createOrUpdateIndex(resources_, "resourceType");
 	}
 
 }
