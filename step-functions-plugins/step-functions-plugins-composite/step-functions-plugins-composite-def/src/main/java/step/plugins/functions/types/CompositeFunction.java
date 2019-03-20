e7264dfc0e699a0733315a1d62afffc7eaa56773
Jérôme Comte <jerome.comte@exense.ch>
2019-03-20 20:54:39 +0100

    Adding switch for local/remote execution of composite KW

--- step-functions-plugins/step-functions-plugins-composite/step-functions-plugins-composite-def/src/main/java/step/plugins/functions/types/CompositeFunction.java
+++ step-functions-plugins/step-functions-plugins-composite/step-functions-plugins-composite-def/src/main/java/step/plugins/functions/types/CompositeFunction.java
@@ -5,6 +5,8 @@ import step.functions.Function;
 public class CompositeFunction extends Function {
 
 	String artefactId;
+	
+	boolean executeLocally = true;
 
 	public String getArtefactId() {
 		return artefactId;
@@ -14,9 +16,17 @@ public class CompositeFunction extends Function {
 		this.artefactId = artefactId;
 	}
 
+	public boolean isExecuteLocally() {
+		return executeLocally;
+	}
+
+	public void setExecuteLocally(boolean executeLocally) {
+		this.executeLocally = executeLocally;
+	}
+
 	@Override
 	public boolean requiresLocalExecution() {
-		return false;
+		return executeLocally;
 	}
 	
 }
