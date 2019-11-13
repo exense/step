9ccb0c7b91c132db2f30f10e74e80d20caadca90
Dorian Cransac <reachme@doriancransac.info>
2019-11-13 18:52:59 +0100

    Upgrading viz deps to alpha-023

--- step-controller/step-controller-server-webapp/src/main/resources/webapp/js/controllers/dashboards.js
+++ step-controller/step-controller-server-webapp/src/main/resources/webapp/js/controllers/dashboards.js
@@ -16,108 +16,17 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
-angular.module('dashboardsControllers',['tables','step', 'viz-dashboard-manager'])
+angular.module('dashboardsControllers',['tables','step', 'viz-session-manager'])
 
 .run(function(ViewRegistry) {
-  ViewRegistry.registerView('dashboards','partials/dashboards/sessionManager.html');
+  ViewRegistry.registerView('dashboards','partials/dashboards/dashboardsController.html');
   ViewRegistry.registerCustomMenuEntry('Dashboards','dashboards');
 })
 
-.controller('SessionManagerCtrl', function($rootScope, $scope, $http, stateStorage, Dialogs, ResourceDialogs, AuthService) {
+.controller('DashboardsController', function($rootScope, $scope, $http, stateStorage, Dialogs, ResourceDialogs, AuthService) {
     stateStorage.push($scope, 'dashboards', {});	
     $scope.authService = AuthService;
     $scope.sessionName = "New Session";
 	$scope.staticPresets = new StaticPresets();
 	$scope.dashboardsendpoint = [];
-	//$scope.dashboardsendpoint.push(new PerformanceDashboard());
-	
-	$scope.deriveEventName = function(sbName) {
-		return sbName.split('.')[1];
-	};
-
-	$scope.$on('sb.dashboard-new', function(event, arg) {
-		if(arg && arg === 'explore'){
-			$scope.$broadcast($scope.deriveEventName(event.name), {displaytype : 'exploded'})
-		}else{
-			$scope.$broadcast($scope.deriveEventName(event.name))
-		}
-	});
-	$scope.$on('sb.dashboard-clear', function(event) {
-		$scope.$broadcast($scope.deriveEventName(event.name))
-	});
-	$scope.$on('sb.dashboard-current-addWidget', function(event) {
-		$scope.$broadcast($scope.deriveEventName(event.name))
-	});
-	$scope.$on('sb.dashboard-current-clearWidgets', function(event) {
-		$scope.$broadcast($scope.deriveEventName(event.name))
-	});
-	$scope.$on('sb.dashboard-configure', function(event) {
-		$scope.$broadcast($scope.deriveEventName(event.name))
-	});
-	$scope.$on('sb.docs', function(event) {
-		$scope.$broadcast($scope.deriveEventName(event.name))
-	});
-	$scope.$on('sb.saveSession', function(event) {
-		$scope.saveSession($scope.sessionName);
-	});
-	$scope.$on('sb.loadSession', function(event) {
-		$scope.loadSession($scope.sessionName);
-	});
-	$scope.$on('sb.deleteSession', function(event) {
-		$scope.deleteSession($scope.sessionName);
-	});
-
-	$scope.saveSession = function(sessionName){
-		console.log($scope.dashboardsendpoint);
-		var serialized = angular.toJson({ name : sessionName, state : $scope.dashboardsendpoint }); 
-		$http.post('/rest/viz/crud/session?name='+sessionName, serialized)
-		.then(function (response) {
-			console.log('response')
-			console.log(response)
-		}, function (response) {
-			console.log('error response')
-			console.log(response)
-		});
-	};
-
-	$scope.loadSession = function(sessionName){
-		$http.get('/rest/viz/crud/session?name='+sessionName)
-		.then(function (response) {
-			if(response && response.data && response.data.state && response.data.state.length > 0){
-				$scope.dashboardsendpoint = response.data.state;
-			}else{
-				$scope.dashboardsendpoint = [];	
-			}
-		}, function (response) {
-			console.log('error response')
-			console.log(response)
-		});
-
-	};
-	
-	$scope.deleteSession = function(sessionName){
-		$http.delete('/rest/viz/crud/session?name='+sessionName)
-		.then(function (response) {
-			console.log('response')
-			console.log(response)
-
-		}, function (response) {
-			console.log('error response')
-			console.log(response)
-		});
-
-	};
-
   })
-.directive('toolbar', function () {
-	return {
-		restrict: 'E',
-		scope:{
-			dashboards: '='
-		},
-		templateUrl: 'partials/dashboards/toolbar.html',
-		controller: function ($scope, $element, $http) {
-
-		}
-	};
-})
