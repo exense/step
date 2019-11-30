1948457f64b0b3480e82eca21e633e3980cfc149
Dorian Cransac <reachme@doriancransac.info>
2019-11-30 14:08:18 +0100

    Adding default project compatibility for repository-enabled workflows

--- step-controller/step-controller-server-webapp/src/main/resources/webapp/js/controllers/repository.js
+++ step-controller/step-controller-server-webapp/src/main/resources/webapp/js/controllers/repository.js
@@ -18,88 +18,127 @@
  *******************************************************************************/
 angular.module('repositoryControllers', [ 'step','dataTable' ])
 
-// This controller is used to force reload of the following Controllers after location change. This is a trick but it works
+//This controller is used to force reload of the following Controllers after location change. This is a trick but it works
 .controller('RepositoryLoadCtrl', function($scope, $location ,$timeout) {
-  $scope.reload = true;
-  $scope.$on('$locationChangeSuccess',function(event) {
-    $scope.reload = false; 
-    $timeout(function(){$scope.reload=true});
-  });
+	$scope.reload = true;
+	$scope.$on('$locationChangeSuccess',function(event) {
+		$scope.reload = false; 
+		$timeout(function(){$scope.reload=true});
+	});
 })
 
 .controller('RepositoryCtrl', [
-    '$rootScope',                           
-  	'$scope',
-  	'$http',
-  	'$location',
-  	'stateStorage',
-  	function($rootScope, $scope, $http, $location, $stateStorage) {
-  	  $stateStorage.push($scope, 'repository', {});
-  	    	  
-  	  if($location.search().user) {
-  	    $rootScope.context.userID = $location.search().user;  	      
-  	  }
-      
-  	  $scope.isolateExecution = $location.search().isolate?$location.search().isolate:false;
-  	  
-  	  if($location.search().repositoryId) {
-  	    $scope.repoRef = {'repositoryID':$location.search().repositoryId,'repositoryParameters':$location.search()};
-  	    $scope.loading = true;
-  	    $http.post("rest/controller/repository/artefact/info",$scope.repoRef).then(
-  	        function(response) {
-  	          $scope.loading = false
-  	          $scope.artefactInfo = response.data;
-  	        }, 
-  	        function errorCallback(response) { 
-  	          $scope.loading = false
-  	          $scope.error = response.data;
-  	        });
-  	    
-  	    $scope.functions = {};  	    
-  	  }
-  	 }
-  	])
-  	  
-.controller('TestSetOverviewCtrl', [
-    '$scope',
-    '$http',
-    '$location',
-    'stateStorage',
-    function($scope, $http, $location, $stateStorage) {
-      $scope.testCaseTable = {};
-      $scope.testCaseTable.columns = [ { "title" : "ID", "visible" : false },
-                                   {"title" : "Name"},
-                                   { "title" : "Status", "width":"80px", "searchmode":"select","render": function ( data, type, row ) {
-                                     return '<div class="text-center reportNodeStatus status-' + data +'">'  +data+ '</div>';
-                                   }} ];
-      $scope.testCaseTable.defaultSelection = "all";
-      
-      $scope.repoRef = {'repositoryID':$location.search().repositoryId,'repositoryParameters':$location.search()};
-      $http.post("rest/controller/repository/report",$scope.repoRef).then(
-          function(response) {
-            var data = response.data;
-            var dataSet = [];
-            var runs = data.runs;
-            for (i = 0; i < runs.length; i++) {
-              dataSet[i] = [ runs[i].testplanName, runs[i].testplanName, runs[i].status];
-            }
-            $scope.testCaseTable.data = dataSet;
-          });
-      $scope.functions.getIncludedTestcases = function() {
-        var selectionMode = $scope.testCaseTable.getSelectionMode();
-        if(selectionMode=='all') {
-          return null;
-        } else if (selectionMode=='custom' || selectionMode=='none') {
-          var includedTestCases = {"by":"name"};
-          var result = [];
-          if($scope.testCaseTable.getRows!=null) {
-            _.each($scope.testCaseTable.getRows(true),function(value){result.push(value[0])});
-          }
-          includedTestCases.list = result;
-          return includedTestCases;          
-        } else {
-          throw "Unsupported selection mode: "+selectionMode;
-        }
-      }
-    }
-])
+	'$rootScope',                           
+	'$scope',
+	'$http',
+	'$location',
+	'stateStorage',
+	'AuthService',
+	function($rootScope, $scope, $http, $location, $stateStorage, AuthService) {
+
+		$scope.session = AuthService.getContext().session;
+
+		$scope.setALMProject = function(projectId) {
+			if(!$scope.session.attributes['step.controller.multitenancy.TenantContext']){
+				$scope.session.attributes['step.controller.multitenancy.TenantContext'] = {};
+			}
+			if(!$scope.session.attributes['step.controller.multitenancy.TenantContext'].contextValues){
+				$scope.session.attributes['step.controller.multitenancy.TenantContext'].contextValues = {};
+			}
+			$scope.session.attributes['step.controller.multitenancy.TenantContext'].contextValues.project = projectId;
+		};
+
+		$scope.reload = true;
+		$scope.prepareProjectSession = function(callback) {
+			
+			/**/
+			//TODO externalize default repository project name and pass via conf 
+			var repositoryProjectName = 'alm'
+			/**/	
+				
+			$http.post('/rest/tenants/project/search', { 'name' : repositoryProjectName}, {headers : {'ignoreContext': 'true'}}).then(function(response){
+				if(response.data.id){
+					$scope.setALMProject(response.data.id);
+					$http.post('rest/tenants/context', {contextValues:$scope.session.attributes['step.controller.multitenancy.TenantContext'].contextValues}).then(function(){
+						callback();
+					});
+				}else{
+					callback();
+				}
+			});
+		};
+
+		$scope.runController = function(){
+
+			$stateStorage.push($scope, 'repository', {});
+
+			if($location.search().user) {
+				$rootScope.context.userID = $location.search().user;  	      
+			}
+
+			$scope.isolateExecution = $location.search().isolate?$location.search().isolate:false;
+
+			if($location.search().repositoryId) {
+				$scope.repoRef = {'repositoryID':$location.search().repositoryId,'repositoryParameters':$location.search()};
+				$scope.loading = true;
+				$http.post("rest/controller/repository/artefact/info",$scope.repoRef).then(
+						function(response) {
+							$scope.loading = false
+							$scope.artefactInfo = response.data;
+						}, 
+						function errorCallback(response) { 
+							$scope.loading = false
+							$scope.error = response.data;
+						});
+
+				$scope.functions = {};  	    
+			}
+		};
+
+		$scope.prepareProjectSession($scope.runController);
+	}
+	])
+
+	.controller('TestSetOverviewCtrl', [
+		'$scope',
+		'$http',
+		'$location',
+		'stateStorage',
+		function($scope, $http, $location, $stateStorage) {
+			$scope.testCaseTable = {};
+			$scope.testCaseTable.columns = [ { "title" : "ID", "visible" : false },
+				{"title" : "Name"},
+				{ "title" : "Status", "width":"80px", "searchmode":"select","render": function ( data, type, row ) {
+					return '<div class="text-center reportNodeStatus status-' + data +'">'  +data+ '</div>';
+				}} ];
+			$scope.testCaseTable.defaultSelection = "all";
+
+			$scope.repoRef = {'repositoryID':$location.search().repositoryId,'repositoryParameters':$location.search()};
+			$http.post("rest/controller/repository/report",$scope.repoRef).then(
+					function(response) {
+						var data = response.data;
+						var dataSet = [];
+						var runs = data.runs;
+						for (i = 0; i < runs.length; i++) {
+							dataSet[i] = [ runs[i].testplanName, runs[i].testplanName, runs[i].status];
+						}
+						$scope.testCaseTable.data = dataSet;
+					});
+			$scope.functions.getIncludedTestcases = function() {
+				var selectionMode = $scope.testCaseTable.getSelectionMode();
+				if(selectionMode=='all') {
+					return null;
+				} else if (selectionMode=='custom' || selectionMode=='none') {
+					var includedTestCases = {"by":"name"};
+					var result = [];
+					if($scope.testCaseTable.getRows!=null) {
+						_.each($scope.testCaseTable.getRows(true),function(value){result.push(value[0])});
+					}
+					includedTestCases.list = result;
+					return includedTestCases;          
+				} else {
+					throw "Unsupported selection mode: "+selectionMode;
+				}
+			}
+		}
+		])
