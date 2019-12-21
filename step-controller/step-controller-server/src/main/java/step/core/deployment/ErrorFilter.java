6bf4419d8c9704356de455ab994f73cdd70b8ca5
Jérôme Comte <jerome.comte@exense.ch>
2019-12-21 12:34:26 +0100

    SED-252 Improving logging of REST services errors

--- step-controller/step-controller-server/src/main/java/step/core/deployment/ErrorFilter.java
+++ step-controller/step-controller-server/src/main/java/step/core/deployment/ErrorFilter.java
@@ -4,13 +4,18 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Secured
 @Provider
 public class ErrorFilter extends AbstractServices implements ExceptionMapper<Exception>  {
 
+	private static final Logger logger = LoggerFactory.getLogger(ErrorFilter.class);
+	
 	@Override
 	public Response toResponse(Exception exception) {
-		exception.printStackTrace();
+		logger.error("Unexpected error while processing request", exception);
 		return Response.status(500).entity("Unexepected server error occurred: "+exception.getClass().getName()+":"+exception.getMessage()).type("text/plain").build();
 	}
 }
