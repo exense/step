45c0209c8f01461eaebd240782718b511778e8a1
Jonathan Rubiero <jonathan.rubiero@exense.ch>
2020-04-20 10:33:21 +0200

    SED-376 - Implemented check on plan simulation execution

--- step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/handlers/SleepHandler.java
+++ step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/handlers/SleepHandler.java
@@ -33,7 +33,9 @@ public class SleepHandler extends ArtefactHandler<Sleep, ReportNode> {
 	@Override
 	protected void execute_(ReportNode node, Sleep testArtefact) {
 		try {
-			Thread.sleep(((Number)testArtefact.getDuration().get()).longValue());
+			if(!context.isSimulation()) {
+				Thread.sleep(((Number)testArtefact.getDuration().get()).longValue());
+			}
 		} catch (NumberFormatException e) {
 			throw new RuntimeException("Unable to parse attribute 'ms' as long.",e);
 		} catch (InterruptedException e) {}
