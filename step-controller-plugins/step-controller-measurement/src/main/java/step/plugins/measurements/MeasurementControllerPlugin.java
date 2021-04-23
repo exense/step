82b98be1121ef5152d07d9b7aaacfad3f27955dd
David Stephan <David.Stephan@exense.ch>
2021-04-23 12:10:15 +0200

    SED-713  supporting grid metric labels configuration

--- step-controller-plugins/step-controller-measurement/src/main/java/step/plugins/measurements/MeasurementControllerPlugin.java
+++ step-controller-plugins/step-controller-measurement/src/main/java/step/plugins/measurements/MeasurementControllerPlugin.java
@@ -14,6 +14,8 @@ import step.grid.TokenWrapperState;
 import step.grid.reports.TokenGroupCapacity;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static step.plugins.measurements.MeasurementPlugin.*;
 
@@ -30,43 +32,51 @@ public class MeasurementControllerPlugin extends AbstractControllerPlugin {
 	@Override
 	public void executionControllerStart(GlobalContext context) throws Exception {
 		super.executionControllerStart(context);
-
 		initGaugeCollectorRegistry(context);
 	}
 
 	protected void initGaugeCollectorRegistry(GlobalContext context) {
 		gaugeCollectorRegistry = GaugeCollectorRegistry.getInstance();
+
 		//Register grid gauge metrics
-		final String[] labels = {"agent_type","url","name"};
+		//Define grouping attributes and corresponds metric labels ($ not supported)
+		List<String> groupBy = new ArrayList<>(Arrays.asList(context.getConfiguration()
+				.getProperty("plugins.grid.monitoring.attributes", "$agenttype,type")
+				.split(",")));
+		if (!groupBy.contains("url")) {groupBy.add("url");}
+		List<String> labels = groupBy.stream().map(s->s.replace("$","")).collect(Collectors.toList());
+		labels.add("name");
 		if (context.get(Grid.class)!=null) {
 			gaugeCollectorRegistry.registerCollector(GridGaugeName, new GaugeCollector(GridGaugeName,
-					"step grid token usage and capacity", labels) {
-				final List<String> groupBy = Arrays.asList(new String[]{"$agenttype", "url"});
+					"step grid token usage and capacity", labels.stream().toArray(String[]::new)) {
 				final GridReportBuilder gridReportBuilder = new GridReportBuilder((Grid) context.get(Grid.class));
-				final Map<String, String> urlToType = new HashMap<>();
+				final Map<String, String[][]> urlToType = new HashMap<>();
 
 				@Override
 				public List<Collector.MetricFamilySamples> collect() {
 					List<TokenGroupCapacity> usageByIdentity = gridReportBuilder.getUsageByIdentity(groupBy);
-					Map<String, String> newUrlToType = new HashMap<>();
+					Map<String, String[][]> newUrlToType = new HashMap<>();
 					for (TokenGroupCapacity tokenGroupCapacity : usageByIdentity) {
 						Integer free = tokenGroupCapacity.getCountByState().get(TokenWrapperState.FREE);
 						free = (free == null) ? 0 : free;
 						int capacity = tokenGroupCapacity.getCapacity();
 						String agentUrl = tokenGroupCapacity.getKey().get("url");
-						String agentType = tokenGroupCapacity.getKey().get("$agenttype");
-						newUrlToType.put(agentUrl, agentType);
 						urlToType.remove(agentUrl);
-						getGauge().labels(agentType,
-								agentUrl, "free").set(Double.valueOf(free));
-						getGauge().labels(agentType,
-								agentUrl, "capacity").set(Double.valueOf(capacity));
+						List<String> labelValues = groupBy.stream().map(s -> Objects.requireNonNullElse(tokenGroupCapacity.getKey().get(s), "")).collect(Collectors.toList());
+						String[] lblValFree = Stream.concat(labelValues.stream(),Stream.of("free")).toArray(String[]::new);
+						String[] lblValCapacity = Stream.concat(labelValues.stream(),Stream.of("capacity")).toArray(String[]::new);
+						getGauge().labels(lblValFree).set(Double.valueOf(free));
+						getGauge().labels(lblValCapacity).set(Double.valueOf(capacity));
+						newUrlToType.put(agentUrl, new String[][]{lblValFree,lblValCapacity});
 					}
+					//Remaining urls from previous iteration doesn't exist anymore, cleaning-up metrics
 					for (String url : urlToType.keySet()) {
-						String type = urlToType.remove(url);
-						getGauge().remove(type, url,"free");
-						getGauge().remove(type,url, "capacity");
+						String[][] labelsAr = urlToType.remove(url);
+						for (int i=0; i < labelsAr.length; i++) {
+							getGauge().remove(labelsAr[i]);
+						}
 					}
+					//save set of agent urls from current iteration
 					urlToType.putAll(newUrlToType);
 					return getGauge().collect();
 				}
