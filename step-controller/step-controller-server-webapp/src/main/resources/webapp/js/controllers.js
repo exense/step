ccb189e850c05d452f3e0326eaddc8a34d7bbe9b
David Stephan <David.Stephan@exense.ch>
2021-07-22 17:18:13 +0200

    SED-671 optim testcase execution table

--- step-controller/step-controller-server-webapp/src/main/resources/webapp/js/controllers.js
+++ step-controller/step-controller-server-webapp/src/main/resources/webapp/js/controllers.js
@@ -339,6 +339,7 @@ tecAdminControllers.directive('executionProgress', ['$http','$timeout','$interva
 				});
 			}
 
+			$scope.showTestCaseCurrentOperation = true;
 			var refreshExecution = function() {
 				$http.get('rest/executions/' + eId).then(function(response) {
 					var data = response.data;
@@ -348,6 +349,14 @@ tecAdminControllers.directive('executionProgress', ['$http','$timeout','$interva
 						}
 					}
 					$scope.execution = data;
+					var showTestCaseCurrentOperation = $scope.execution.parameters.find(o => o.key === 'step.executionView.testcases.current-operations');
+					if (showTestCaseCurrentOperation) {
+						if (showTestCaseCurrentOperation.value.toLowerCase() === 'true') {
+							$scope.showTestCaseCurrentOperation = true;
+						} else {
+							$scope.showTestCaseCurrentOperation = false;
+						}
+					}
 					// Set actual execution to the tab title
 					$scope.updateTabTitle()(eId,data);
 				});        
