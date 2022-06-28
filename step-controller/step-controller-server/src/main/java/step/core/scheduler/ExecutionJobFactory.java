8e65dca26c73fc0292bf9a1373c0b75c689d493a
Jérôme Comte <jerome.comte@exense.ch>
2022-06-28 14:02:28 +0200

    SED-1103 Implementing execution grid client (work in progress)

--- step-controller/step-controller-server/src/main/java/step/core/scheduler/ExecutionJobFactory.java
+++ step-controller/step-controller-server/src/main/java/step/core/scheduler/ExecutionJobFactory.java
@@ -1,23 +1,24 @@
 /*******************************************************************************
  * Copyright (C) 2020, exense GmbH
- *  
+ *
  * This file is part of STEP
- *  
+ *
  * STEP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * STEP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package step.core.scheduler;
 
+import ch.exense.commons.app.Configuration;
 import org.bson.types.ObjectId;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
@@ -25,47 +26,57 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.spi.JobFactory;
 import org.quartz.spi.TriggerFiredBundle;
-
 import step.core.GlobalContext;
 import step.core.controller.ControllerSetting;
 import step.core.controller.ControllerSettingAccessor;
 import step.core.execution.ExecutionEngine;
 
+import java.lang.reflect.Constructor;
+
 public class ExecutionJobFactory implements JobFactory {
 
-	private final ExecutionEngine executionEngine;
-	private final ControllerSettingAccessor controllerSettingAccessor;
-	private final ExecutionTaskAccessor executionTaskAccessor;
-	
-	public ExecutionJobFactory(GlobalContext context, ExecutionEngine executionEngine) {
-		super();
-		controllerSettingAccessor = context.require(ControllerSettingAccessor.class);
-		this.executionEngine = executionEngine;
-		this.executionTaskAccessor = context.getScheduleAccessor();
-	}
+    private final ExecutionEngine executionEngine;
+    private final ControllerSettingAccessor controllerSettingAccessor;
+    private final ExecutionTaskAccessor executionTaskAccessor;
+    private final Configuration configuration;
 
-	@Override
-	public Job newJob(TriggerFiredBundle arg0, Scheduler arg1) throws SchedulerException {
-		JobDataMap data = arg0.getJobDetail().getJobDataMap();
-		String executionID;
-		if(data.containsKey(Executor.EXECUTION_ID)) {
-			executionID = data.getString(Executor.EXECUTION_ID);
-		} else {
-			String executionTaskID = data.getString(Executor.EXECUTION_TASK_ID);
-			ExecutiontTaskParameters executiontTaskParameters = executionTaskAccessor.get(new ObjectId(executionTaskID));
-			
-			ControllerSetting schedulerUsernameSetting = controllerSettingAccessor.getSettingByKey("scheduler_execution_username");
-			if(schedulerUsernameSetting != null) {
-				String schedulerUsername = schedulerUsernameSetting.getValue();
-				if(schedulerUsername != null && schedulerUsername.trim().length()>0) {
-					// Override the execution user if the setting scheduler_execution_username is set
-					executiontTaskParameters.getExecutionsParameters().setUserID(schedulerUsername);
-				}
-			}
-			
-			executionID = executionEngine.initializeExecution(executiontTaskParameters);
-		}
-		 
-		return new ExecutionJob(executionEngine, executionID);
-	}
+    public ExecutionJobFactory(GlobalContext context, ExecutionEngine executionEngine) {
+        super();
+        configuration = context.getConfiguration();
+        controllerSettingAccessor = context.require(ControllerSettingAccessor.class);
+        this.executionEngine = executionEngine;
+        this.executionTaskAccessor = context.getScheduleAccessor();
+    }
+
+    @Override
+    public Job newJob(TriggerFiredBundle arg0, Scheduler arg1) {
+        JobDataMap data = arg0.getJobDetail().getJobDataMap();
+        String executionID;
+        if (data.containsKey(Executor.EXECUTION_ID)) {
+            executionID = data.getString(Executor.EXECUTION_ID);
+        } else {
+            String executionTaskID = data.getString(Executor.EXECUTION_TASK_ID);
+            ExecutiontTaskParameters executiontTaskParameters = executionTaskAccessor.get(new ObjectId(executionTaskID));
+
+            ControllerSetting schedulerUsernameSetting = controllerSettingAccessor.getSettingByKey("scheduler_execution_username");
+            if (schedulerUsernameSetting != null) {
+                String schedulerUsername = schedulerUsernameSetting.getValue();
+                if (schedulerUsername != null && schedulerUsername.trim().length() > 0) {
+                    // Override the execution user if the setting scheduler_execution_username is set
+                    executiontTaskParameters.getExecutionsParameters().setUserID(schedulerUsername);
+                }
+            }
+
+            executionID = executionEngine.initializeExecution(executiontTaskParameters);
+        }
+
+        String executorClassName = configuration.getProperty("controller.execution.job.classname", ExecutionJob.class.getName());
+        try {
+            Class<?> aClass = Class.forName(executorClassName);
+            Constructor<?> constructor = aClass.getConstructor(ExecutionEngine.class, String.class);
+            return (Job) constructor.newInstance(executionEngine, executionID);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while initializing executor " + executorClassName, e);
+        }
+    }
 }
