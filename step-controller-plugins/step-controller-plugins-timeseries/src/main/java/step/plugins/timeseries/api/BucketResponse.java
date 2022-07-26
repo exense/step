4a88b2e87f6912b02adbf70d13dbe69c76f2454e
Lucian Baciu <lucian.baciu@exense.ch>
2022-07-26 16:15:34 +0300

    updating the entities

---
+++ step-controller-plugins/step-controller-plugins-timeseries/src/main/java/step/plugins/timeseries/api/BucketResponse.java
@@ -0,0 +1,55 @@
+package step.plugins.timeseries.api;
+
+import step.core.timeseries.BucketAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BucketResponse {
+
+    private final long begin;
+    private final BucketAttributes attributes;
+    private final long count;
+    private final long sum;
+    private final long min;
+    private final long max;
+    private final Map<Integer, Long> pclPrecisions;
+
+    public BucketResponse(long begin, BucketAttributes attributes, long count, long sum, long min, long max, Map<Integer, Long> pclPrecisions) {
+        this.begin = begin;
+        this.attributes = attributes;
+        this.count = count;
+        this.sum = sum;
+        this.min = min;
+        this.max = max;
+        this.pclPrecisions = pclPrecisions;
+    }
+
+    public long getBegin() {
+        return begin;
+    }
+
+    public BucketAttributes getAttributes() {
+        return attributes;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public long getSum() {
+        return sum;
+    }
+
+    public long getMin() {
+        return min;
+    }
+
+    public long getMax() {
+        return max;
+    }
+
+    public Map<Integer, Long> getPclPrecisions() {
+        return pclPrecisions;
+    }
+}
