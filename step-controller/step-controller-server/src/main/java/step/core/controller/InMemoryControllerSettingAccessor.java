d3b889e7ccb92b5b8043fafde853a27a26b42865
Igor Egorov <igor.egorov@exense.ch>
2023-02-01 20:56:14 +0000

    SED-1533: fixed InMemoryControllerSettingAccessor

--- step-controller/step-controller-server/src/main/java/step/core/controller/InMemoryControllerSettingAccessor.java
+++ step-controller/step-controller-server/src/main/java/step/core/controller/InMemoryControllerSettingAccessor.java
@@ -1,11 +1,6 @@
 package step.core.controller;
 
-import org.bson.types.ObjectId;
 import step.core.collections.inmemory.InMemoryCollection;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class InMemoryControllerSettingAccessor extends ControllerSettingAccessorImpl implements ControllerSettingAccessor {
 
@@ -20,66 +15,6 @@ public class InMemoryControllerSettingAccessor extends ControllerSettingAccessor
 			return copy;
 		}
 		return null;
-	}
-
-	// in all methods below we need to return a clone, because otherwise a caller can get and modify the in-memory object via simple setters
-
-
-	@Override
-	public ControllerSetting getSettingByKey(String key) {
-		return copy(super.getSettingByKey(key));
-	}
-
-	@Override
-	public ControllerSetting get(ObjectId id) {
-		return copy(super.get(id));
-	}
-
-	@Override
-	public ControllerSetting findByCriteria(Map<String, String> criteria) {
-		return copy(super.findByCriteria(criteria));
-	}
-
-	@Override
-	public Stream<ControllerSetting> findManyByCriteria(Map<String, String> criteria) {
-		return super.findManyByCriteria(criteria).map(this::copy);
-	}
-
-	@Override
-	public ControllerSetting findByAttributes(Map<String, String> attributes) {
-		return copy(super.findByAttributes(attributes));
-	}
-
-	@Override
-	public Spliterator<ControllerSetting> findManyByAttributes(Map<String, String> attributes) {
-		// TODO: copy value in spliterator
-		return super.findManyByAttributes(attributes);
-	}
-
-	@Override
-	public ControllerSetting findByAttributes(Map<String, String> attributes, String attributesMapKey) {
-		return copy(super.findByAttributes(attributes, attributesMapKey));
-	}
-
-	@Override
-	public Spliterator<ControllerSetting> findManyByAttributes(Map<String, String> attributes, String attributesMapKey) {
-		// TODO: copy value in spliterator
-		return super.findManyByAttributes(attributes, attributesMapKey);
-	}
-
-	@Override
-	public Iterator<ControllerSetting> getAll() {
-		return this.stream().map(this::copy).iterator();
-	}
-
-	@Override
-	public Stream<ControllerSetting> stream() {
-		return super.stream().map(this::copy);
-	}
-
-	@Override
-	public List<ControllerSetting> getRange(int skip, int limit) {
-		return super.getRange(skip, limit).stream().map(this::copy).collect(Collectors.toList());
 	}
 
 }
