0109e3c7fbdcea0c3843fc95b0a5fdbb9d716eee
Igor Egorov <igor.egorov@exense.ch>
2023-03-28 05:23:42 +0000

    SED-554: json schema in keyword annotation

--- step-functions/step-functions-package/src/main/java/step/functions/packages/handlers/KeywordJsonSchemaReader.java
+++ step-functions/step-functions-package/src/main/java/step/functions/packages/handlers/KeywordJsonSchemaReader.java
@@ -1,13 +1,17 @@
 package step.functions.packages.handlers;
 
-import jakarta.json.Json;
-import jakarta.json.JsonException;
-import jakarta.json.JsonObject;
+import jakarta.json.*;
 import jakarta.json.stream.JsonParsingException;
 import step.handlers.javahandler.JsonSchema;
+import step.handlers.javahandler.JsonSchemaProperty;
+import step.handlers.javahandler.JsonSchemaPropertyRef;
 import step.handlers.javahandler.Keyword;
 
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class KeywordJsonSchemaReader {
 
@@ -21,15 +25,65 @@ public class KeywordJsonSchemaReader {
 		if (useTextJsonSchema) {
 			return readJsonSchemaFromPlainText(keywordAnnotation.schema(), methodName);
 		} else if (useAnnotatedJsonSchema) {
-			return readJsonSchemaFromAnnotationKeyword(keywordAnnotation.jsonSchema()[0]);
+			return readJsonSchemaFromAnnotationKeyword(keywordAnnotation.jsonSchema()[0], methodName);
 		} else {
 			return createEmptyJsonSchema();
 		}
 	}
 
-	private JsonObject readJsonSchemaFromAnnotationKeyword(JsonSchema jsonSchema) {
+	private JsonObject readJsonSchemaFromAnnotationKeyword(JsonSchema jsonSchema, String methodName) throws JsonSchemaPreparationException {
 		// TODO: build the json schema
-		return Json.createObjectBuilder().build();
+		Map<String, JsonSchemaProperty> propertyRefs = new HashMap<>();
+		for (JsonSchemaPropertyRef jsonSchemaPropertyRef : jsonSchema.propertiesRefs()) {
+			propertyRefs.put(jsonSchemaPropertyRef.id(), jsonSchemaPropertyRef.property());
+		}
+
+		JsonObjectBuilder topLevelBuilder = Json.createObjectBuilder();
+
+		if (jsonSchema.title() != null && !jsonSchema.title().isEmpty()) {
+			topLevelBuilder.add("title", jsonSchema.title());
+		}
+		if (jsonSchema.description() != null && !jsonSchema.description().isEmpty()) {
+			topLevelBuilder.add("description", jsonSchema.description());
+		}
+		JsonArrayBuilder propertiesBuilder = Json.createArrayBuilder();
+		topLevelBuilder.add("properties", propertiesBuilder);
+		List<String> requiredProperties = new ArrayList<>();
+		for (JsonSchemaProperty property : jsonSchema.properties()) {
+			JsonObjectBuilder propertyBuilder = Json.createObjectBuilder();
+			while (property.propertyRef() != null && !property.propertyRef().isEmpty()) {
+				String ref = property.propertyRef();
+				property = propertyRefs.get(ref);
+				if (property == null) {
+					throw new JsonSchemaPreparationException("Unable to resolve json schema property ref '  " + ref + " ' for keyword '" + methodName + "'");
+				}
+				// TODO: check for cyclic references
+			}
+			JsonObjectBuilder propertyParamsBuilder = Json.createObjectBuilder();
+
+			propertyBuilder.add(property.name(), propertyParamsBuilder);
+
+			if (property.type() == null || property.type().isEmpty()) {
+				throw new JsonSchemaPreparationException("Property type is undefined for json schema property '" + property.type() + "' for keyword '" + methodName + "'");
+			}
+			propertyParamsBuilder.add("type", property.type());
+
+			if (property.defaultV() != null && !property.defaultV().isEmpty()) {
+				propertyParamsBuilder.add("default", property.defaultV());
+			}
+
+			propertiesBuilder.add(propertyBuilder);
+			if (property.required()) {
+				requiredProperties.add(property.name());
+			}
+		}
+		JsonArrayBuilder requiredBuilder = Json.createArrayBuilder();
+		for (String requiredProperty : requiredProperties) {
+			requiredBuilder.add(requiredProperty);
+		}
+		topLevelBuilder.add("required", requiredBuilder);
+
+		return topLevelBuilder.build();
 	}
 
 	protected JsonObject createEmptyJsonSchema() {
@@ -47,4 +101,5 @@ public class KeywordJsonSchemaReader {
 			throw new JsonSchemaPreparationException("Unknown error in the schema for keyword '" + methodName + "'. The error was: " + e.getMessage());
 		}
 	}
+
 }
