77c639e40296545d36b85890695218203f2636b5
Igor Egorov <igor.egorov@exense.ch>
2023-09-06 23:42:32 +0300

    SED-2391: fix incorrect parameter name

--- step-maven-plugin/step-commons-maven-plugin/src/main/java/step/plugins/maven/AbstractRunAutomationPackagesMojo.java
+++ step-maven-plugin/step-commons-maven-plugin/src/main/java/step/plugins/maven/AbstractRunAutomationPackagesMojo.java
@@ -63,7 +63,7 @@ public abstract class AbstractRunAutomationPackagesMojo extends AbstractStepPlug
 	@Parameter(property = "step-run-auto-packages.lib-artifact-classifier")
 	private String libArtifactClassifier;
 
-	@Parameter(property = "step-upload-keywords.lib-tracking-attr", required = false)
+	@Parameter(property = "step-run-auto-packages.lib-tracking-attr", required = false)
 	private String libTrackingAttr;
 
 	protected void executeBundleOnStep(Map<String, Object> executionContext) throws MojoExecutionException {
