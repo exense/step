4abeefc75d4862dd8ebfddbe025ce5b1478ec75f
David Stephan <David.Stephan@exense.ch>
2023-09-15 16:35:27 +0200

    SED-2416 Attach meta data to TS BucketResponse

--- step-controller-plugins/step-controller-plugins-timeseries/src/main/java/step/plugins/timeseries/TimeSeriesHandler.java
+++ step-controller-plugins/step-controller-plugins-timeseries/src/main/java/step/plugins/timeseries/TimeSeriesHandler.java
@@ -306,6 +306,7 @@ public class TimeSeriesHandler {
                             .withSum(b.getSum())
                             .withThroughputPerHour(3600 * 1000 * b.getCount() / (b.getEnd() - b.getBegin()))
                             .withPclValues(request.getPercentiles().stream().collect(Collectors.toMap(p -> p, b::getPercentile)))
+                            .withAttributes(b.getAttributes())
                             .build();
                 }
                 bucketResponses.add(bucketResponse);
