7b56d905c92dfb9ff3741c27e4ababc5794e2a8a
Igor Egorov <igor.egorov@exense.ch>
2023-10-01 21:39:24 +0300

    SED-2331: configurable launch for JMeter tests

--- step-automation-packages/step-automation-packages-junit/src/test/java/step/engine/plugins/AutomationPackageKeywordsPluginTest.java
+++ step-automation-packages/step-automation-packages-junit/src/test/java/step/engine/plugins/AutomationPackageKeywordsPluginTest.java
@@ -1,12 +1,12 @@
 package step.engine.plugins;
 
-import org.junit.Ignore;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import step.junit.categories.LocalJMeter;
 import step.junit.runner.Step;
 import step.junit.runners.annotations.Plans;
 
-// TODO: how to disable the test if jmeter_home env variable is undefined?
-@Ignore
+@Category(LocalJMeter.class)
 @RunWith(Step.class)
 @Plans({"testAutomation.plan"})
 public class AutomationPackageKeywordsPluginTest {
