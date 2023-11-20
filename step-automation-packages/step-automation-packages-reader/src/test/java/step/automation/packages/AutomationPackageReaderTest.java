72e5c0c653112d7d124754394c8392732a287ca1
Igor Egorov <igor.egorov@exense.ch>
2023-11-20 19:34:34 +0300

    SED-2533: fix tests

--- step-automation-packages/step-automation-packages-reader/src/test/java/step/automation/packages/AutomationPackageReaderTest.java
+++ step-automation-packages/step-automation-packages-reader/src/test/java/step/automation/packages/AutomationPackageReaderTest.java
@@ -20,7 +20,7 @@ public class AutomationPackageReaderTest {
 
     @Test
     public void testReadFromPackage() throws AutomationPackageReadingException {
-        File automationPackageJar = new File("src/test/resources/step/automation/packages/yaml/testPack2.jar");
+        File automationPackageJar = new File("src/test/resources/step/automation/packages/yaml/testpack2.jar");
 
         AutomationPackage automationPackage = reader.readAutomationPackageFromJarFile(automationPackageJar);
         assertNotNull(automationPackage);
