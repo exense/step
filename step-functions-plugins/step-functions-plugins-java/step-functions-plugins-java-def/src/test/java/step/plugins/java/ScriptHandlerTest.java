7c9c7eb07ae052ab86d983af6f3a054a4f8f75d8
Christoph Langguth <christoph.langguth@exense.ch>
2024-03-06 17:18:35 +0100

    SED-2937 Make script tests use same package as implementation

--- step-functions-plugins/step-functions-plugins-java/step-functions-plugins-java-def/src/test/java/step/plugins/java/ScriptHandlerTest.java
+++ step-functions-plugins/step-functions-plugins-java/step-functions-plugins-java-def/src/test/java/step/plugins/java/ScriptHandlerTest.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package step.handlers.scripthandler;
+package step.plugins.java;
 
 import java.io.IOException;
 import java.util.ArrayList;
