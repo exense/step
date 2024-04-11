67e6a11af87e1b743a328ca607cb89706b486674
Jérôme Comte <jerome.comte@exense.ch>
2024-04-11 16:13:25 +0200

    SED-3010 Fixing JUnit

--- step-plans/step-plans-core/src/main/java/step/core/artefacts/handlers/ArtefactHashGenerator.java
+++ step-plans/step-plans-core/src/main/java/step/core/artefacts/handlers/ArtefactHashGenerator.java
@@ -1,21 +1,12 @@
 package step.core.artefacts.handlers;
 
 import jakarta.xml.bind.DatatypeConverter;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class ArtefactHashGenerator {
-
-    private static MessageDigest messageDigest;
-
-    public ArtefactHashGenerator() {
-        try {
-            messageDigest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public String generateArtefactHash(String currentPath, String artefactId) {
         return generateArtefactHash(getPath(currentPath, artefactId));
@@ -26,8 +17,9 @@ public class ArtefactHashGenerator {
     }
 
     public String generateArtefactHash(String path) {
-        messageDigest.update(path.getBytes());
-        byte[] digest = messageDigest.digest();
+        // The result of DigestUtils.getMd5Digest() is not thread safe.
+        // TODO use ThreadLocal or cache if necessary
+        byte[] digest = DigestUtils.getMd5Digest().digest(path.getBytes());
         return DatatypeConverter.printHexBinary(digest);
     }
 
