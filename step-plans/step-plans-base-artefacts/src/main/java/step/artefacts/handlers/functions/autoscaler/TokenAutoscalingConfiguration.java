2fd65b5ee7e80df1050b70fcf5b0ac14ec49e26f
Jonathan Rubiero <jonathan.rubiero@exense.ch>
2024-04-19 07:37:20 +0200

    SED-2997 removed K8s Client API from step-plan-base-artefacts (PR comment)

--- step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/handlers/functions/autoscaler/TokenAutoscalingConfiguration.java
+++ step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/handlers/functions/autoscaler/TokenAutoscalingConfiguration.java
@@ -1,14 +1,10 @@
 package step.artefacts.handlers.functions.autoscaler;
 
-import io.kubernetes.client.openapi.models.V1ConfigMapList;
-
 import java.util.Map;
 
 public class TokenAutoscalingConfiguration {
 
     public Map<String, Map<String, String>> availableTokenPools;
-
-    public V1ConfigMapList availableConfigMapList;
 
     public TokenAutoscalingConfiguration() {}
 
