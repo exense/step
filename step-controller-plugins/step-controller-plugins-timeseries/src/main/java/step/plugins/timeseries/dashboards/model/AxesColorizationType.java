19c9a3b2ffa2f3e8205d218f6e8330974ab75324
Lucian Baciu <103116491+lucianbaciu@users.noreply.github.com>
2024-04-24 17:11:08 +0300

    SED-3008 Ability to specify colorization type for charts (ONLY_STROKE… (#243)
    * SED-3008-Ability to specify colorization type for charts (ONLY_STROKE or FILL)
    
    * SED-3008-added default colorization STOKE

---
+++ step-controller-plugins/step-controller-plugins-timeseries/src/main/java/step/plugins/timeseries/dashboards/model/AxesColorizationType.java
@@ -0,0 +1,8 @@
+package step.plugins.timeseries.dashboards.model;
+
+public enum AxesColorizationType {
+    
+    STROKE, // only the stroke of each series is colorized
+    FILL // each series is filled with color
+    
+}
