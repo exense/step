d657f8189c714cd28efd0925c896404e15a724a3
Igor Egorov <igor.egorov@exense.ch>
2024-05-10 23:30:54 +0300

    SED-2669: keep original file name in AutomationPackageArchive

--- step-automation-packages/step-automation-packages-controller/src/test/java/step/automation/packages/AutomationPackageArchiveTest.java
+++ step-automation-packages/step-automation-packages-controller/src/test/java/step/automation/packages/AutomationPackageArchiveTest.java
@@ -28,7 +28,7 @@ public class AutomationPackageArchiveTest {
     public void isAutomationPackage() throws AutomationPackageReadingException, IOException {
         File automationPackageJar = new File("src/test/resources/samples/step-automation-packages-sample2.jar");
 
-        try (AutomationPackageArchive validPackage = new AutomationPackageArchive(automationPackageJar, automationPackageJar.getName())) {
+        try (AutomationPackageArchive validPackage = new AutomationPackageArchive(automationPackageJar)) {
             Assert.assertTrue(validPackage.hasAutomationPackageDescriptor());
 
             JsonNode actualDescriptor = yamlObjectMapper.readTree(validPackage.getDescriptorYaml());
@@ -46,7 +46,7 @@ public class AutomationPackageArchiveTest {
         }
 
         File malformedPackageJar = new File("src/test/resources/samples/step-automation-packages-malformed.jar");
-        try (AutomationPackageArchive malformed = new AutomationPackageArchive(malformedPackageJar, malformedPackageJar.getName())) {
+        try (AutomationPackageArchive malformed = new AutomationPackageArchive(malformedPackageJar)) {
             boolean isAutomationPackage = malformed.hasAutomationPackageDescriptor();
             if (isAutomationPackage) {
                 JsonNode malformedDescriptor = yamlObjectMapper.readTree(malformed.getDescriptorYaml());
