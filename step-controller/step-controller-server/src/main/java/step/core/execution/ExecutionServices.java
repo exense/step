f505a4c63a358bfe2e730b0b12ed721827e116ec
David Stephan <david.stephan@exense.ch>
2024-07-15 14:44:03 +0200

    SED-3271 Bulk restart should on executed on behalf of original user

--- step-controller/step-controller-server/src/main/java/step/core/execution/ExecutionServices.java
+++ step-controller/step-controller-server/src/main/java/step/core/execution/ExecutionServices.java
@@ -73,19 +73,23 @@ public class ExecutionServices extends AbstractStepAsyncServices {
 	@Secured(right="plan-execute")
 	public String execute(ExecutionParameters executionParams) {
 		checkRightsOnBehalfOf("plan-execute", executionParams.getUserID());
-		applyUserIdFromSession(executionParams);
+		applyUserIdFromSessionIfNotSpecified(executionParams);
 		return getScheduler().execute(executionParams);
 	}
 
-	private void applyUserIdFromSession(ExecutionParameters executionParams) {
+	private void applyUserIdFromSessionIfNotSpecified(ExecutionParameters executionParams) {
 		// explicitly defined user id has a priority
 		if (executionParams.getUserID() == null) {
-			Session<User> session = getSession();
-			if (session != null) {
-				User user = session.getUser();
-				if (user != null) {
-					executionParams.setUserID(user.getUsername());
-				}
+			applyUserIdFromSession(executionParams);
+		}
+	}
+
+	private void applyUserIdFromSession(ExecutionParameters executionParams) {
+		Session<User> session = getSession();
+		if (session != null) {
+			User user = session.getUser();
+			if (user != null) {
+				executionParams.setUserID(user.getUsername());
 			}
 		}
 	}
