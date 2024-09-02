f8964cb5606d62fc7daa02820398e9f5475a7539
Jérôme Comte <jerome.comte@exense.ch>
2024-09-02 11:53:45 +0200

    SED-3176 Adding sts event logs to agent provisioning logs

--- step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/handlers/functions/autoscaler/AgentProvisioningLogs.java
+++ step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/handlers/functions/autoscaler/AgentProvisioningLogs.java
@@ -1,7 +1,9 @@
 package step.artefacts.handlers.functions.autoscaler;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class AgentProvisioningLogs extends HashMap<String, AgentProvisioningLog> {
 
+    public List<String> stsEvents;
 }
