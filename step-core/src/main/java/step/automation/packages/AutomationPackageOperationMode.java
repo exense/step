2834bc443cc5c30e76bf0336f3d439733aa90d3b
Christoph Langguth <christoph.langguth@exense.ch>
2024-09-23 15:32:20 +0200

    SED-2949 Adding Javadoc

--- step-core/src/main/java/step/automation/packages/AutomationPackageOperationMode.java
+++ step-core/src/main/java/step/automation/packages/AutomationPackageOperationMode.java
@@ -1,5 +1,9 @@
 package step.automation.packages;
 
+/**
+ * Automation package operation mode, indicating if an AutomationPackageManager
+ * and its operations are in main (=production), isolated, or local context.
+ */
 public enum AutomationPackageOperationMode {
     MAIN,
     ISOLATED,
