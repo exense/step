a2d9d406154616bcf8090b0b6941140fb08b33a5
Igor Egorov <118996755+iegorov777@users.noreply.github.com>
2024-12-10 11:35:44 +0300

    SED-3533 packaging of ap should support to exclude files (#359)
    * SED-3533: packaging of AP should support to exclude files
    
    * SED-3533: packaging of AP should support to exclude files
    
    * SED-3533: packaging of AP should support to exclude files
    
    * SED-3533 Fixing and refactoring ApIgnoreFileFilter
    
    * SED-3533: packaging of AP should support to exclude files
    
    * SED-3533 bumping framework
    
    ---------
    
    Co-authored-by: David Stephan <david.stephan@exense.ch>

--- step-cli/step-cli-launcher/src/main/java/step/cli/StepConsole.java
+++ step-cli/step-cli-launcher/src/main/java/step/cli/StepConsole.java
@@ -18,9 +18,9 @@
  ******************************************************************************/
 package step.cli;
 
+import ch.exense.commons.io.FileHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.zeroturnaround.zip.ZipUtil;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -30,6 +30,7 @@ import step.client.controller.ControllerServicesClient;
 import step.client.credentials.ControllerCredentials;
 import step.core.Constants;
 import step.core.Version;
+import step.cli.apignore.ApIgnoreFileFilter;
 
 import java.io.File;
 import java.io.IOException;
@@ -39,6 +40,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 
@@ -51,6 +53,8 @@ import java.util.function.Supplier;
 public class StepConsole implements Callable<Integer> {
 
     public static final String REQUIRED_ERR_MESSAGE = "Illegal parameters. One of the following options is required: '%s'";
+
+    private static final String AP_IGNORE_NAME = ".apignore";
 
     private static final Logger log = LoggerFactory.getLogger(StepConsole.class);
 
@@ -181,12 +185,19 @@ public class StepConsole implements Callable<Integer> {
                         // check if the folder is AP (contains the yaml descriptor)
                         checkApFolder(file);
 
+                        Function<File, Boolean> fileFilter = null;
+                        File apIgnoreFile = new File(file, AP_IGNORE_NAME);
+                        if (apIgnoreFile.exists()) {
+                            ApIgnoreFileFilter gitIgnore = new ApIgnoreFileFilter(file.toPath(), apIgnoreFile.toPath());
+                            fileFilter = file1 -> !file1.getName().equals(AP_IGNORE_NAME) && gitIgnore.accept(file1.toPath());
+                        }
+
                         File tempDirectory = Files.createTempDirectory("stepcli").toFile();
                         tempDirectory.deleteOnExit();
                         File tempFile = new File(tempDirectory, file.getName() + ".stz");
                         tempFile.deleteOnExit();
                         log.info("Preparing AP archive: {}", tempFile.getAbsolutePath());
-                        ZipUtil.pack(file, tempFile);
+                        FileHelper.zip(file, tempFile, fileFilter);
                         return tempFile;
                     } else {
                         return file;
