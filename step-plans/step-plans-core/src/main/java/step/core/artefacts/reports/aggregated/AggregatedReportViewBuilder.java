ca0ae4329c51edc3252478942763c3dab0ce2e25
David Stephan <david.stephan@exense.ch>
2025-02-10 14:57:15 +0100

    SED-3721 PR feedbacks

--- step-plans/step-plans-core/src/main/java/step/core/artefacts/reports/aggregated/AggregatedReportViewBuilder.java
+++ step-plans/step-plans-core/src/main/java/step/core/artefacts/reports/aggregated/AggregatedReportViewBuilder.java
@@ -2,6 +2,8 @@ package step.core.artefacts.reports.aggregated;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import step.core.artefacts.reports.ReportNode;
 import step.core.artefacts.reports.ReportNodeAccessor;
 import step.core.artefacts.reports.resolvedplan.ResolvedPlanNode;
@@ -14,27 +16,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class AggregatedReportViewBuilder {
+
+    private static final Logger logger = LoggerFactory.getLogger(AggregatedReportViewBuilder.class);
+    public static final String EXECUTION_REPORT_AGGREGATED_TREE_RESOLVE_SINGLE_INSTANCE = "execution.report.aggregated-tree.resolve-single-instance";
 
     private final String executionId;
     private final ExecutionAccessor executionAccessor;
     private final ResolvedPlanNodeAccessor resolvedPlanNodeAccessor;
     private final ReportNodeTimeSeries reportNodesTimeSeries;
     private final ReportNodeAccessor reportNodeAccessor;
+    private final boolean defaultResolveSingleInstanceReport;
 
     public AggregatedReportViewBuilder(ExecutionEngineContext executionEngineContext, String executionId) {
         this.executionId = executionId;
         this.executionAccessor = executionEngineContext.getExecutionAccessor();
         this.resolvedPlanNodeAccessor = executionEngineContext.require(ResolvedPlanNodeAccessor.class);
         this.reportNodeAccessor = executionEngineContext.getReportNodeAccessor();
+        this.defaultResolveSingleInstanceReport =  executionEngineContext.getConfiguration().getPropertyAsBoolean(EXECUTION_REPORT_AGGREGATED_TREE_RESOLVE_SINGLE_INSTANCE, true);
         reportNodesTimeSeries = executionEngineContext.require(ReportNodeTimeSeries.class);
     }
 
     public AggregatedReportView buildAggregatedReportView() {
-        return buildAggregatedReportView(new AggregatedReportViewRequest(null));
+        return buildAggregatedReportView(new AggregatedReportViewRequest(null, null));
     }
 
     public AggregatedReportView buildAggregatedReportView(AggregatedReportViewRequest request) {
@@ -50,10 +56,13 @@ public class AggregatedReportViewBuilder {
 
     public static class AggregatedReportViewRequest {
         public final ReportNodeTimeSeries.Range range;
+        public final Boolean resolveSingleInstanceReport;
 
         @JsonCreator
-        public AggregatedReportViewRequest(@JsonProperty("range") ReportNodeTimeSeries.Range range) {
+        public AggregatedReportViewRequest(@JsonProperty("range") ReportNodeTimeSeries.Range range,
+                                           @JsonProperty("resolveSingleInstanceReport") Boolean resolveSingleInstanceReport) {
             this.range = range;
+            this.resolveSingleInstanceReport = resolveSingleInstanceReport;
         }
     }
 
@@ -64,14 +73,28 @@ public class AggregatedReportViewBuilder {
         String artefactHash = resolvedPlanNode.artefactHash;
         Map<String, Long> countByStatus = reportNodesTimeSeries.queryByExecutionIdAndArtefactHash(executionId, artefactHash, request.range);
         ReportNode singleInstanceReportNode = null;
-        if (countByStatus.values().stream().reduce(0L, Long::sum) == 1) {
+        if (resolveSingleReport(request) && countByStatus.values().stream().reduce(0L, Long::sum) == 1) {
             singleInstanceReportNode = getSingleReportNodeInstance(executionId, artefactHash, request.range);
         }
         return new AggregatedReportView(resolvedPlanNode.artefact, artefactHash, countByStatus, children, resolvedPlanNode.parentSource, singleInstanceReportNode);
     }
 
+    private boolean resolveSingleReport(AggregatedReportViewRequest request) {
+        return (request.resolveSingleInstanceReport != null) ? request.resolveSingleInstanceReport : defaultResolveSingleInstanceReport;
+    }
+
     private ReportNode getSingleReportNodeInstance(String executionId, String artefactHash, ReportNodeTimeSeries.Range range) {
-        return reportNodeAccessor.getReportNodesByArtefactHash(executionId, artefactHash, (range != null) ? range.from : null, (range != null) ? range.to : null, 0, 1).findFirst().orElse(null);
+        Long from = (range != null) ? range.from : null;
+        Long to = (range != null) ? range.to : null;
+        List<ReportNode> reports = reportNodeAccessor.getReportNodesByArtefactHash(executionId, artefactHash, from, to, 0, 2).collect(Collectors.toList());
+        if (reports.size() == 1) {
+            return reports.get(0);
+        } else {
+            String reportCountMsg = (reports.isEmpty()) ? "no" : "more than one";
+            logger.error("Unexpected number of report nodes found. Found {} report for execution id {}, artefactHash: {} and range: {} to {}",
+                    reportCountMsg, executionId, artefactHash, from, to);
+            return null;
+        }
     }
 
 }
