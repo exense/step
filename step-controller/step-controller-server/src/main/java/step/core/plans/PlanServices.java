c1984e6b2458d1e51eff638a5f6a95c2b906eb6c
David Stephan <david.stephan@exense.ch>
2025-03-04 10:23:00 +0100

    SED-3347 fixing indentation

--- step-controller/step-controller-server/src/main/java/step/core/plans/PlanServices.java
+++ step-controller/step-controller-server/src/main/java/step/core/plans/PlanServices.java
@@ -101,13 +101,13 @@ public class PlanServices extends AbstractEntityServices<Plan> {
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Secured(right = "{entity}-write")
 	public Plan newPlanFromYaml(String yamlBody) {
-        try {
-            return yamlPlanReader.readYamlPlan(new ByteArrayInputStream(yamlBody.getBytes()));
-        } catch (Exception e) {
+		try {
+			return yamlPlanReader.readYamlPlan(new ByteArrayInputStream(yamlBody.getBytes()));
+		} catch (Exception e) {
 			log.error("Deserialization error while reading providing YAML plan.", e);
 			throw new ControllerServiceException("Deserialization error while reading the Yaml plan: " + e.getMessage(), e);
-        }
-    }
+		}
+	}
 
 	@Override
 	protected Plan beforeSave(Plan entity) {
