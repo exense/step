61c4a715b14d544bd4b1871d7868011a331dc7b2
David Stephan <David.Stephan@exense.ch>
2025-04-10 14:20:20 +0200

    SED-3887 after sequence in plain text cause import errors (#426)
    * SED-3887 afterSequence in Plain text cause import errors
    
    * SED-3887 bug fixing
    
    * SED-3887 bug fixing
    
    * SED-3887 PR feedbacks
    
    * SED-3887 Refactoring based on PR feedbacks
    
    * SED-3887 Refactoring based on PR feedbacks

--- step-plans/step-plans-parser/src/test/java/step/plans/nl/parser/PlanParserTest.java
+++ step-plans/step-plans-parser/src/test/java/step/plans/nl/parser/PlanParserTest.java
@@ -25,8 +25,10 @@ import org.junit.Test;
 import org.junit.Assert;
 import step.artefacts.*;
 import step.artefacts.ThreadGroup;
+import step.core.artefacts.AbstractArtefact;
 import step.core.plans.Plan;
 import step.plans.nl.RootArtefactType;
+import step.repositories.parser.StepsParser;
 import step.repositories.parser.StepsParser.ParsingException;
 
 public class PlanParserTest {
@@ -166,5 +168,39 @@ public class PlanParserTest {
 				"-\n"+
 				"End", null);
 		Assert.assertEquals(ForEachBlock.class, plan.getRoot().getClass());
+	}
+
+	@Test
+	public void testComplexPlans() throws StepsParser.ParsingException {
+
+		PlanParser parser = new PlanParser();
+
+		Plan parsedPlan = parser.parse(getClass().getResourceAsStream("complexPlan.plan"), RootArtefactType.Sequence);
+		AbstractArtefact root = parsedPlan.getRoot();
+		Assert.assertEquals(1,root.getChildren().size());
+
+		ForBlock forBlock = (ForBlock) root.getChildren().get(0);
+		Assert.assertEquals("For loop", forBlock.getAttributes().get("name"));
+
+		Sequence wrappingSequence = (Sequence) forBlock.getChildren().get(0);
+		Assert.assertEquals("Sequence", wrappingSequence.getAttributes().get("name"));
+
+		Assert.assertEquals(3, wrappingSequence.getChildren().size());
+
+		CallFunction callFunction = (CallFunction) wrappingSequence.getChildren().get(0);
+		Assert.assertEquals("Call a keyword", callFunction.getAttributes().get("name"));
+		Assert.assertEquals(1,callFunction.getChildren().size());
+
+		Assert.assertEquals("some echo in iteration", wrappingSequence.getChildren().get(1).getAttributes().get("name"));
+		Assert.assertEquals("some other echo in iteration", wrappingSequence.getChildren().get(2).getAttributes().get("name"));
+
+		Echo echo = (Echo) wrappingSequence.getAfter().getSteps().get(0);
+		Assert.assertEquals("some echo in afterSequence", echo.getAttributes().get("name"));
+
+		Echo echoBeforeSequence = (Echo) wrappingSequence.getBefore().getSteps().get(0);
+		Assert.assertEquals("some echo in before sequence", echoBeforeSequence.getAttributes().get("name"));
+
+		Echo echoAfter = (Echo) forBlock.getAfter().getSteps().get(0);
+		Assert.assertEquals("some echo in after section", echoAfter.getAttributes().get("name"));
 	}
 }
