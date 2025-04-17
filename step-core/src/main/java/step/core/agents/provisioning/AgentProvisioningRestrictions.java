8057f17f3b2baa8b51ecfe7e7b2da1a023f196b8
Christoph Langguth <christoph.langguth@exense.ch>
2025-04-17 15:58:16 +0200

    SED-3764 Implement autoprovisioning license limits (WIP)

---
+++ step-core/src/main/java/step/core/agents/provisioning/AgentProvisioningRestrictions.java
@@ -0,0 +1,21 @@
+package step.core.agents.provisioning;
+
+/**
+ * Class containing restrictions for the provisioning.
+ * This is meant for situations where licensing is enabled,
+ * and the number of concurrent agents and/or tokens that
+ * the controller accepts might be limited.
+ *
+ * Limits are independent of each other, and if a limit is
+ * present (not null), the controller will not handle
+ * registration requests beyond the limit, instead ignoring
+ * the requests.
+ *
+ * It is therefore advised to either only provision accordingly
+ * to stay within the stated limits, or to reject a request
+ * altogether if any of the limits would be exceeded.
+ */
+public class AgentProvisioningRestrictions {
+    public Integer agentCountLimit;
+    public Integer tokenCountLimit;
+}
