9ca76e832ef1c9c2c0a02a0e5b975cf557faa63e
rubij <jonathan.rubiero@exense.ch>
2025-04-25 14:59:20 +0200

    SED-3949 reintroduced missing report node classes

---
+++ step-plans/step-plans-base-artefacts/src/main/java/step/artefacts/reports/BeforeThreadReportNode.java
@@ -0,0 +1,14 @@
+package step.artefacts.reports;
+
+import step.core.artefacts.reports.ReportNode;
+/**
+ * Required for the housekeeping job (executions created prior to the deprecation / removal of this class)
+ */
+@Deprecated
+public class BeforeThreadReportNode extends ReportNode {
+
+	@Override
+	public boolean setVariableInParentScope() {
+		return true;
+	}
+}
