18bb3b485af9dbec938710c9a08537daeb4ae71b
David Stephan <david.stephan@exense.ch>
2025-05-06 13:48:02 +0200

    SED-3958 PR feedbacks updates

--- step-controller/step-controller-server/src/main/java/step/core/scheduler/ExecutionJob.java
+++ step-controller/step-controller-server/src/main/java/step/core/scheduler/ExecutionJob.java
@@ -29,11 +29,14 @@ import step.core.controller.ControllerSetting;
 import step.core.controller.ControllerSettingAccessor;
 import step.core.execution.ExecutionEngine;
 
+import java.util.Objects;
+
 import static step.automation.packages.AutomationPackageLocks.AUTOMATION_PACKAGE_READ_LOCK_TIMEOUT_SECS;
 import static step.automation.packages.AutomationPackageLocks.AUTOMATION_PACKAGE_READ_LOCK_TIMEOUT_SECS_DEFAULT;
 
 public class ExecutionJob implements Job {
-	
+
+	public static final String SETTING_KEY_SCHEDULER_EXECUTION_USERNAME = "scheduler_execution_username";
 	private final ExecutionEngine executionEngine;
 	private final ExecutionTaskAccessor executionTaskAccessor;
 	private final ControllerSettingAccessor controllerSettingAccessor;
@@ -42,10 +45,10 @@ public class ExecutionJob implements Job {
 	private final String executionTaskID;
 
 	public ExecutionJob(ExecutionEngine executionEngine, ExecutionTaskAccessor executionTaskAccessor, ControllerSettingAccessor controllerSettingAccessor, AutomationPackageLocks lock, String executionId, String executionTaskID) {
-		this.executionEngine = executionEngine;
-		this.executionTaskAccessor = executionTaskAccessor;
-		this.controllerSettingAccessor = controllerSettingAccessor;
-		this.automationPackageLocks = lock;
+		this.executionEngine = Objects.requireNonNull(executionEngine);
+		this.executionTaskAccessor = Objects.requireNonNull(executionTaskAccessor);
+		this.controllerSettingAccessor = Objects.requireNonNull(controllerSettingAccessor);
+		this.automationPackageLocks = Objects.requireNonNull(lock);
 		this.executionId = executionId;
 		this.executionTaskID = executionTaskID;
 	}
@@ -59,7 +62,7 @@ public class ExecutionJob implements Job {
 			try {
 				ExecutiontTaskParameters executiontTaskParameters = executionTaskAccessor.get(new ObjectId(executionTaskID));
 				if (executiontTaskParameters == null) {
-					throw new JobExecutionException("The execution task parameters for the provided schedule ID '" + executionTaskID + "' doesn't exists in the database anymore. This error may happen for race condition when deleting a schedule right when its job is triggered.");
+					throw new JobExecutionException("The execution task parameters for schedule ID '" + executionTaskID + "' were not found in the database. This may be due to a race condition where a schedule is being deleted at the same time as its job is triggered.");
 				}
 				//Try to get the read lock on automation package even before creating the execution data, otherwise data get out dated
 				automationPackageLockID = (String) executiontTaskParameters.getCustomField(AutomationPackageEntity.AUTOMATION_PACKAGE_ID);
@@ -77,11 +80,11 @@ public class ExecutionJob implements Job {
 					executiontTaskParameters = executionTaskAccessor.get(new ObjectId(executionTaskID));
 					// Error handling in case the schedule got deleted in between
 					if (executiontTaskParameters == null) {
-						throw new JobExecutionException("The execution task parameters for the provided schedule ID '" + executionTaskID + "' doesn't exists in the database anymore. This error may happen for race condition when deleting a schedule right when its job is triggered.");
+						throw new JobExecutionException("The execution task parameters for schedule ID '" + executionTaskID + "' were not found in the database. This may be due to a race condition where a schedule is being deleted at the same time as its job is triggered.");
 					}
 				}
 
-				ControllerSetting schedulerUsernameSetting = controllerSettingAccessor.getSettingByKey("scheduler_execution_username");
+				ControllerSetting schedulerUsernameSetting = controllerSettingAccessor.getSettingByKey(SETTING_KEY_SCHEDULER_EXECUTION_USERNAME);
 				if (schedulerUsernameSetting != null) {
 					String schedulerUsername = schedulerUsernameSetting.getValue();
 					if (schedulerUsername != null && schedulerUsername.trim().length() > 0) {
