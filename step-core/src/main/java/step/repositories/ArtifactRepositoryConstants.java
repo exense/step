dd166692adf9e8defb9aa3072e7e6288365cd40a
Igor Egorov <igor.egorov@exense.ch>
2025-06-04 14:30:07 +0300

    SED-3965: upload AP via maven snippet (fix)

--- step-core/src/main/java/step/repositories/ArtifactRepositoryConstants.java
+++ step-core/src/main/java/step/repositories/ArtifactRepositoryConstants.java
@@ -43,4 +43,25 @@ public class ArtifactRepositoryConstants {
 
     // Parameters in Resource Artifact Repository
     public static final String RESOURCE_PARAM_RESOURCE_ID = "resourceId";
+
+    public static final String MAVEN_EMPTY_SETTINGS =
+            "<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\"\n" +
+                    "          xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                    "          xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd\">\n" +
+                    "    <activeProfiles>\n" +
+                    "        <activeProfile>default</activeProfile>\n" +
+                    "    </activeProfiles>\n" +
+                    "\n" +
+                    "    <profiles>\n" +
+                    "        <profile>\n" +
+                    "            <id>default</id>\n" +
+                    "            <repositories>\n" +
+                    "                <repository>\n" +
+                    "                    <id>central</id>\n" +
+                    "                    <url>https://repo1.maven.org/maven2/</url>\n" +
+                    "                </repository>\n" +
+                    "            </repositories>\n" +
+                    "        </profile>\n" +
+                    "    </profiles>\n" +
+                    "</settings>";
 }
