f64cca2663f3965ff569b9760256d16c3b93666f
Igor Egorov <igor.egorov@exense.ch>
2025-06-10 20:04:31 +0300

    SED-4014: fix overriding in AP fragments

--- step-automation-packages/step-automation-packages-junit5/src/test/java/step/junit5/runner/StepAutomationPackageRunAllTest.java
+++ step-automation-packages/step-automation-packages-junit5/src/test/java/step/junit5/runner/StepAutomationPackageRunAllTest.java
@@ -1,11 +1,9 @@
 package step.junit5.runner;
 
 import org.junit.experimental.categories.Category;
-import org.junit.jupiter.api.Disabled;
 import step.junit.categories.LocalJMeter;
 import step.junit.runners.annotations.ExecutionParameters;
 
-@Disabled
 @Category(LocalJMeter.class)
 @ExecutionParameters({"PARAM_EXEC","Value","PARAM_EXEC2","Value","PARAM_EXEC3","Value"})
 public class StepAutomationPackageRunAllTest extends StepJUnit5 {
