73b9ebc25c8ed3f1e227f4a38676f4c895981196
lucianbaciu <lucian.baciu@exense.ch>
2025-07-02 13:12:08 +0300

    SED-3999-make execution range params optional

--- step-plans/step-plans-core/src/main/java/step/core/execution/model/ExecutionAccessorImpl.java
+++ step-plans/step-plans-core/src/main/java/step/core/execution/model/ExecutionAccessorImpl.java
@@ -68,10 +68,15 @@ public class ExecutionAccessorImpl extends AbstractAccessor<Execution> implement
 		return collectionDriver.find(filter, null, null, null, 0).collect(Collectors.toList());
 	}
 
-	public List<Execution> findByCritera(Map<String, String> criteria, long start, long end, SearchOrder order, int skip, int limit) {
+	public List<Execution> findByCritera(Map<String, String> criteria, Long start, Long end, SearchOrder order, int skip, int limit) {
 		List<Filter> filters = new ArrayList<>();
-		filters.add(Filters.gte("startTime", start));
-		filters.add(Filters.lte("endTime", end));
+		if (start != null) {
+			filters.add(Filters.gte("startTime", start));
+		}
+		if (end != null) {
+			filters.add(Filters.lte("endTime", end));
+		}
+
 		criteria.forEach((k, v) -> {
 			filters.add(Filters.equals(k, v));
 		});
