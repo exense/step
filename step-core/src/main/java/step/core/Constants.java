af8f48ab2453845547fa45e0d4cbcebdabdb4b5c
David Stephan <david.stephan@exense.ch>
2025-07-15 08:39:36 +0200

    SED-000 bumping Step version to 3.29.0

--- step-core/src/main/java/step/core/Constants.java
+++ step-core/src/main/java/step/core/Constants.java
@@ -19,7 +19,7 @@
 package step.core;
 
 public interface Constants {
-    String STEP_API_VERSION_STRING = "3.28.0";
+    String STEP_API_VERSION_STRING = "3.29.0";
     Version STEP_API_VERSION = new Version(STEP_API_VERSION_STRING);
 
     String STEP_YAML_SCHEMA_VERSION_STRING = "1.1.1";
