051b800711de80d566f133bc51a352fcc25da869
Christoph Langguth <christoph.langguth@exense.ch>
2025-09-10 12:20:41 +0200

    SED-4195 unexpected quota errors: refuse uploads instead of using global quotas

--- step-controller/step-controller-base-plugins/src/main/java/step/plugins/streaming/QuotaCheckers.java
+++ step-controller/step-controller-base-plugins/src/main/java/step/plugins/streaming/QuotaCheckers.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import step.constants.StreamingConstants;
 import step.core.artefacts.reports.ReportNode;
 import step.core.variables.VariablesManager;
+import step.streaming.common.QuotaExceededException;
 import step.streaming.common.StreamingResourceUploadContext;
 
 import java.util.Map;
@@ -23,7 +24,7 @@ public class QuotaCheckers {
         this.globalLimits = globalLimits;
     }
 
-    private QuotaLimits determineEffectiveLimits(String executionId, StreamingResourceUploadContext uploadContext) {
+    private QuotaLimits determineEffectiveLimits(String executionId, StreamingResourceUploadContext uploadContext) throws QuotaExceededException {
         // TODO: Step 30+ adjust for per-project overridable limits
         if (!globalLimits.runtimeOverridable) {
             return globalLimits;
@@ -31,8 +32,9 @@ public class QuotaCheckers {
         try {
             VariablesManager variables = (VariablesManager) uploadContext.getAttributes().get(StreamingConstants.AttributeNames.VARIABLES_MANAGER);
             if (variables == null) {
+                // Note that this should not normally happen, it would indicate some kind of serious bug
                 logger.warn("No variables manager found while calculating quotas for execution id {}, this should not happen; returning global limits", executionId);
-                return globalLimits;
+                throw new QuotaExceededException("UNEXPECTED: No variables manager found, quota check impossible");
             }
             ReportNode reportNode = (ReportNode) uploadContext.getAttributes().get(StreamingConstants.AttributeNames.REPORT_NODE);
             QuotaLimits overridden = QuotaLimits.fromVariables(variables, reportNode, globalLimits);
@@ -42,24 +44,46 @@ public class QuotaCheckers {
                 }
             }
             return overridden;
+        } catch (QuotaExceededException e) {
+            throw e;
         } catch (Exception e) {
-            // better safe than sorry
-            logger.error("Error while determining quotas for execution id {}, falling back to default system quota limits", executionId, e);
-            return globalLimits;
+            // Again, this should not normally happen, it would indicate some kind of serious bug
+            logger.error("Unexpected error while determining quotas for execution id {}, refusing upload", executionId, e);
+            throw new QuotaExceededException("UNEXPECTED error when determining quotas: " + e.getMessage() );
         }
     }
 
-    public QuotaChecker getForExecution(String executionId, StreamingResourceUploadContext uploadContext) {
-        Optional<QuotaChecker> entry = perExecution.computeIfAbsent(executionId, id -> {
-            QuotaLimits limits = determineEffectiveLimits(id, uploadContext);
-            if (limits.isUnlimited()) {
-                // indicate that no quota enforcement is required
-                return Optional.empty();
-            }
-            return Optional.of(new QuotaChecker(id, limits));
-        });
-        // empty -> null
-        return entry.orElse(null);
+    // Required because we can't throw checked exceptions in closures
+    private static final class QuotaExceededUnchecked extends RuntimeException {
+        QuotaExceededUnchecked(QuotaExceededException cause) { super(cause); }
+        // That synchronized modifier is present in super.getCause() anyway, so we just cleanly override the existing signature.
+        // This doesn't add any additional overhead.
+        @Override public synchronized QuotaExceededException getCause() {
+            return (QuotaExceededException) super.getCause();
+        }
+    }
+
+    // Note: there are no errors expected to occur, but in case there are, we throw an exception that will effectively refuse the uploads
+    public QuotaChecker getForExecution(String executionId, StreamingResourceUploadContext uploadContext) throws QuotaExceededException {
+        try {
+            Optional<QuotaChecker> entry = perExecution.computeIfAbsent(executionId, id -> {
+                try {
+                    QuotaLimits limits = determineEffectiveLimits(id, uploadContext);
+                    if (limits.isUnlimited()) {
+                        // indicate that no quota enforcement is required
+                        return Optional.empty();
+                    }
+                    return Optional.of(new QuotaChecker(id, limits));
+                } catch (QuotaExceededException e) {
+                    // Note that this should not normally happen, it would indicate some kind of serious bug
+                    throw new QuotaExceededUnchecked(e);
+                }
+            });
+            // empty -> null
+            return entry.orElse(null);
+        } catch (QuotaExceededUnchecked e) {
+            throw e.getCause();
+        }
     }
 
     public void setForResource(String resourceId, QuotaChecker quotaChecker) {
