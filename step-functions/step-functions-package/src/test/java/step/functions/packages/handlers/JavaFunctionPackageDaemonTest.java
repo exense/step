206fc81448f8fc2535d30f21ad474ee3b3261db4
David Stephan <David.Stephan@exense.ch>
2025-09-18 14:49:21 +0200

    SED-4172 step-keyword-api-support-routing-definition (#500)
    * SED-4172 Step Keyword API support routing definition
    
    * SED-4172 PR feedbacks
    
    * SED-4172 additional Junit tests
    
    * SED-4172 bug fixing
    
    * SED-4172 clarify and correct routing definition priorities
    
    * SED-4172 step-keyword-api-support-routing-definition

--- step-functions/step-functions-package/src/test/java/step/functions/packages/handlers/JavaFunctionPackageDaemonTest.java
+++ step-functions/step-functions-package/src/test/java/step/functions/packages/handlers/JavaFunctionPackageDaemonTest.java
@@ -1,8 +1,10 @@
 package step.functions.packages.handlers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -20,9 +22,16 @@ public class JavaFunctionPackageDaemonTest {
 		DiscovererParameters discovererParameters = new DiscovererParameters();
 		discovererParameters.setPackageLocation(testResource.getAbsolutePath());
 		FunctionList functions = daemon.getFunctions(discovererParameters);
-		assertEquals(3, functions.getFunctions().size());
+		assertEquals(5, functions.getFunctions().size());
 
 		Function timeout = functions.getFunctions().stream().filter(f -> f.getAttribute(AbstractOrganizableObject.NAME).equals("MyKeywordWithTimeout")).findFirst().get();
 		assertEquals(100L,(long) timeout.getCallTimeout().get());
+
+		Function myKeywordWithRoutingToController = functions.getFunctions().stream().filter(f -> f.getAttribute(AbstractOrganizableObject.NAME).equals("MyKeywordWithRoutingToController")).findFirst().get();
+		assertTrue(myKeywordWithRoutingToController.isExecuteLocally());
+
+		Function myKeywordWithRoutingCriteria = functions.getFunctions().stream().filter(f -> f.getAttribute(AbstractOrganizableObject.NAME).equals("MyKeywordWithRoutingCriteria")).findFirst().get();
+		Map<String, String> expectedRoutingCriteria = Map.of("OS", "WINDOWS", "TYPE", "PLAYWRIGHT");
+		assertEquals(expectedRoutingCriteria, myKeywordWithRoutingCriteria.getTokenSelectionCriteria());
 	}
 }
