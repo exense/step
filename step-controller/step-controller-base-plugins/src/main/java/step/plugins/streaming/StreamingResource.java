dfd0c7ec979e25565f26c178a9dd02434600eefe
Christoph Langguth <christoph.langguth@exense.ch>
2025-10-30 13:04:04 +0100

    SED-4309 change streaming collection db name (#536)

--- step-controller/step-controller-base-plugins/src/main/java/step/plugins/streaming/StreamingResource.java
+++ step-controller/step-controller-base-plugins/src/main/java/step/plugins/streaming/StreamingResource.java
@@ -5,7 +5,7 @@ import step.core.objectenricher.EnricheableObject;
 import step.streaming.common.StreamingResourceStatus;
 
 public class StreamingResource extends AbstractOrganizableObject implements EnricheableObject {
-    public static final String COLLECTION_NAME = "streaming-resources";
+    public static final String COLLECTION_NAME = "streaming_resources";
     public static final String ATTRIBUTE_EXECUTION_ID = "executionId";
     public String filename;
     public String mimeType;
