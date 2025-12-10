732a312ee3cb704fd9f1c218a86b56840c3b00de
David Stephan <David.Stephan@exense.ch>
2025-12-10 20:57:00 +0100

    SED-4439 Optimize reporting request and query performance for Step 29… (#569)
    SED-4439 Optimize reporting request and query performance for Step 29 branch

--- step-plans/step-plans-core/src/main/java/step/core/timeseries/TimeSeriesCollectionsSettings.java
+++ step-plans/step-plans-core/src/main/java/step/core/timeseries/TimeSeriesCollectionsSettings.java
@@ -167,7 +167,7 @@ public class TimeSeriesCollectionsSettings {
     }
 
     public static TimeSeriesCollectionsSettings readSettings(Configuration configuration, String collectionName) {
-        long mainResolution = getPropertyAsLong(configuration, TIME_SERIES_MAIN_RESOLUTION, collectionName, 1000L);
+        long mainResolution = getPropertyAsLong(configuration, TIME_SERIES_MAIN_RESOLUTION, collectionName, 5000L);
         validateMainResolutionParam(mainResolution);
         return new TimeSeriesCollectionsSettings()
                 .setMainResolution(mainResolution)
