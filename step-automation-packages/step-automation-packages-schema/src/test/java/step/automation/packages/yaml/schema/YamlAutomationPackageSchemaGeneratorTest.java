73bd212cf2665031eeba10a48dc313841cecdbfe
Cyril Misev <cyril.misev@exense.ch>
2026-09-24 14:31:39 +0200

    SED-4905 fix renewed schema export

--- step-automation-packages/step-automation-packages-schema/src/test/java/step/automation/packages/yaml/schema/YamlAutomationPackageSchemaGeneratorTest.java
+++ step-automation-packages/step-automation-packages-schema/src/test/java/step/automation/packages/yaml/schema/YamlAutomationPackageSchemaGeneratorTest.java
@@ -11,8 +11,6 @@ import step.handlers.javahandler.jsonschema.JsonSchemaPreparationException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static step.automation.packages.yaml.YamlAutomationPackageVersions.ACTUAL_JSON_SCHEMA_PATH;
 
@@ -35,7 +33,6 @@ public class YamlAutomationPackageSchemaGeneratorTest {
         log.info("GENERATED SCHEMA:");
         log.info(currentSchema.toPrettyString());
 
-        Files.writeString(Path.of("/Users/cyril/exense/step-backend/step/step-automation-packages/step-automation-packages-yaml/src/main/resources/step/automation/packages/yaml/step-automation-package-schema-os-1.3.0.json"), currentSchema.toPrettyString());
         String errorMessage = "Published schema doesn't match to the actual one. To fix the test you need to publish " +
             "the generated schema printed above and actualize the published schema in current test";
         Assert.assertEquals(errorMessage, publishedSchema.toPrettyString(), currentSchema.toPrettyString());
